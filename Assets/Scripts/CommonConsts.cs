f8faeba8b29578f466cc2ff65404af94a3c27589
Neomaster X <neomasterx@outlook.com>
2025-08-11 00:11:32 +0300

    CommonConsts.InputAxis.*

---
+++ Assets/Scripts/CommonConsts.cs
@@ -0,0 +1,9 @@
+public static class CommonConsts
+{
+  public static class InputAxis
+  {
+    public const string Horizontal = nameof(Horizontal);
+    public const string Vertical = nameof(Vertical);
+    public const string Jump = nameof(Jump);
+  }
+}
