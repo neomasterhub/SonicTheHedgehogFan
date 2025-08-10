a52f7521769d786e88b656d5e3d20edab50fdbee
Neomaster X <neomasterx@outlook.com>
2025-08-11 00:45:48 +0300

    SonicController: jump.

--- Assets/Scripts/SonicController.cs
+++ Assets/Scripts/SonicController.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class SonicController : MonoBehaviour
 {
+  private bool _isGrounded;
   private Rigidbody2D _rb;
   private Vector2 _velocity;
 
+  [Header("Horizontal Movement")]
   public float Acceleration = 10f;
-  public float MaxSpeed = 8f;
   public float Friction = 5f;
+  public float MaxSpeed = 8f;
+
+  [Header("Jump & Gravity")]
+  public float GravityDown = 40f;
+  public float GravityUp = 15f;
+  public float JumpSpeed = 10f;
+  public float MaxFallSpeed = -30f;
+
+  [Header("Ground Settings")]
+  public float GroundCheckDistance = 0.6f;
+  public LayerMask GroundLayer;
 
   private void Start()
   {
@@ -36,6 +48,43 @@ public class SonicController : MonoBehaviour
       }
     }
 
-    _rb.linearVelocity = new Vector2(_velocity.x, _rb.linearVelocity.y);
+    _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, GroundCheckDistance, GroundLayer);
+
+    var velocityY = _rb.linearVelocity.y;
+    if (_isGrounded)
+    {
+      if (Input.GetButtonDown(CommonConsts.InputAxis.Jump))
+      {
+        velocityY = JumpSpeed;
+      }
+      else
+      {
+        // To prevent falling through the ground.
+        velocityY = Mathf.Max(velocityY, 0);
+      }
+    }
+    else
+    {
+      if (velocityY > 0)
+      {
+        if (Input.GetButton(CommonConsts.InputAxis.Jump))
+        {
+          // Lighter gravity while holding jump.
+          velocityY -= GravityUp * Time.deltaTime;
+        }
+        else
+        {
+          velocityY -= GravityDown * Time.deltaTime;
+        }
+      }
+      else
+      {
+        velocityY -= GravityDown * Time.deltaTime;
+      }
+
+      velocityY = Mathf.Max(velocityY, MaxFallSpeed);
+    }
+
+    _rb.linearVelocity = new Vector2(_velocity.x, velocityY);
   }
 }
