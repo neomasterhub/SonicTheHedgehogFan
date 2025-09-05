6c6309ab65d19282c4f2d0adc1778faff88f34ad
neomaster <neomasterx@outlook.com>
2025-09-05 19:52:31 +0300

    #5 Improve SonicConsts class.

--- Assets/Scripts/SonicConsts.cs
+++ Assets/Scripts/SonicConsts.cs
@@ -1,11 +1,5 @@
 public static class SonicConsts
 {
-  public enum Pose
-  {
-    Crouching,
-    Standing,
-  }
-
   public static class AnimatorParameterNames
   {
     public const string Speed = nameof(Speed);
@@ -13,8 +7,9 @@ public static class SonicConsts
 
   public static class AnimatorStateNames
   {
-    public const string Idle = nameof(Idle);
+    public const string Crouching = nameof(Crouching);
     public const string Bored = nameof(Bored);
+    public const string Idle = nameof(Idle);
     public const string Waiting = nameof(Waiting);
     public const string Walking = nameof(Walking);
   }
