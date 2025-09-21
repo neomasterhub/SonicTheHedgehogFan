ab261298134f1f291de9d2dcda5d4d9f288a6b16
neomaster <neomasterx@outlook.com>
2025-09-21 23:28:07 +0300

    #10 Player view manager: update animator

--- Assets/Scripts/PlayerViewManager.cs
+++ Assets/Scripts/PlayerViewManager.cs
@@ -2,18 +2,27 @@ using UnityEngine;
 
 public class PlayerViewManager
 {
+  private readonly Animator _animator;
   private readonly InputInfo _inputInfo;
+  private readonly PlayerSpeedManager _playerSpeedManager;
   private readonly SpriteRenderer _spriteRenderer;
 
-  public PlayerViewManager(InputInfo inputInfo, SpriteRenderer spriteRenderer)
+  public PlayerViewManager(
+    Animator animator,
+    InputInfo inputInfo,
+    PlayerSpeedManager playerSpeedManager,
+    SpriteRenderer spriteRenderer)
   {
+    _animator = animator;
     _inputInfo = inputInfo;
+    _playerSpeedManager = playerSpeedManager;
     _spriteRenderer = spriteRenderer;
   }
 
   public void Update()
   {
     RotateSprite();
+    UpdateAnimator();
   }
 
   private void RotateSprite()
@@ -26,5 +35,17 @@ public class PlayerViewManager
     {
       _spriteRenderer.flipX = true;
     }
+  }
+
+  private void UpdateAnimator()
+  {
+    var speedXAbs = Mathf.Abs(_playerSpeedManager.SpeedX);
+
+    _animator.SetFloat(AnimatorParameterNames.Speed, speedXAbs);
+  }
+
+  private static class AnimatorParameterNames
+  {
+    public const string Speed = nameof(Speed);
   }
 }
