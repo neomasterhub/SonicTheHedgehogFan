02335cb7dc5ce4d9e8a3dd22c3c1e3802eeba5fb
Neomaster X <neomasterx@outlook.com>
2026-03-19 06:14:26 +0300

    #149 PlayerViewRotatorInput ctor

--- Assets/Scripts/Player/PlayerViewRotatorInput.cs
+++ Assets/Scripts/Player/PlayerViewRotatorInput.cs
@@ -4,4 +4,12 @@ public readonly struct PlayerViewRotatorInput
   public readonly float GroundSpeed;
   public readonly float StandingStraightGroundSpeedZone;
   public readonly PlayerState PlayerState;
+
+  public PlayerViewRotatorInput(float groundAngleDeg, float groundSpeed, float standingStraightGroundSpeedZone, PlayerState playerState)
+  {
+    GroundAngleDeg = groundAngleDeg;
+    GroundSpeed = groundSpeed;
+    StandingStraightGroundSpeedZone = standingStraightGroundSpeedZone;
+    PlayerState = playerState;
+  }
 }
