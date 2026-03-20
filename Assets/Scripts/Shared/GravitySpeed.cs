46fbcaee0859bff6ddce144fefe9a6b9bc18acac
Neomaster X <neomasterx@outlook.com>
2026-03-20 19:24:31 +0300

    #165 GravitySpeed

---
+++ Assets/Scripts/Shared/GravitySpeed.cs
@@ -0,0 +1,11 @@
+public readonly struct GravitySpeed
+{
+  public readonly float Up;
+  public readonly float Down;
+
+  public GravitySpeed(float up, float down)
+  {
+    Up = up;
+    Down = down;
+  }
+}
