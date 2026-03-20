d5606705a97e74886500bed2204100037f3103c0
Neomaster X <neomasterx@outlook.com>
2026-03-20 06:21:21 +0300

    #163 Utils/SpeedProvider

--- Assets/Scripts/Utils/SpeedProvider.cs
+++ Assets/Scripts/Utils/SpeedProvider.cs
@@ -2,13 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class SlopeFactorSpeedProvider
+public class SpeedProvider
 {
   private readonly Dictionary<Func<bool>, Func<float>> _condition2speed = new();
 
   public Func<float> Default { get; set; } = () => 0;
 
-  public SlopeFactorSpeedProvider Add(Func<bool> condition, Func<float> speed)
+  public SpeedProvider Add(Func<bool> condition, Func<float> speed)
   {
     _condition2speed.Add(condition, speed);
     return this;
