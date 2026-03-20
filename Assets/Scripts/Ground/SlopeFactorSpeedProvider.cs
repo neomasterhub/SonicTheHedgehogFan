bb7ded4bbd73c9af8e9560ff9a7d7042183a3f4e
Neomaster X <neomasterx@outlook.com>
2026-03-20 04:38:26 +0300

    #158 ref: SlopeFactorSpeedProvider

--- Assets/Scripts/Ground/SlopeFactorSpeedProvider.cs
+++ Assets/Scripts/Ground/SlopeFactorSpeedProvider.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class SlopeFactorSpeedProvider
 {
-  private readonly Dictionary<GroundSide, Func<float, float, float>> _factorAngleSpeed = new();
+  private readonly Dictionary<Func<bool>, Func<float>> _condition2speed = new();
 
-  public SlopeFactorSpeedProvider(Dictionary<GroundSide, Func<float, float, float>> factorAngleSpeed)
+  public Func<float> Default { get; set; } = () => 0;
+
+  public SlopeFactorSpeedProvider Add(Func<bool> condition, Func<float> speed)
   {
-    _factorAngleSpeed = factorAngleSpeed;
+    _condition2speed.Add(condition, speed);
+    return this;
   }
 
-  public Func<float, float, float> this[GroundSide groundSide]
+  public float FirstTriggeredOrDefault()
   {
-    get => _factorAngleSpeed[groundSide];
-    set => _factorAngleSpeed[groundSide] = value;
+    return (_condition2speed.FirstOrDefault(x => x.Key()).Value ?? Default)();
   }
 }
