da96cb14e4913d60b82db131a321d015ddd74016
Neomaster X <neomasterx@outlook.com>
2026-03-20 19:40:59 +0300

    #165 Update SetSpeed_Airborne_Gravity()

--- Assets/Scripts/Player/PlayerSpeedManager.cs
+++ Assets/Scripts/Player/PlayerSpeedManager.cs
@@ -6,6 +6,7 @@ public class PlayerSpeedManager
   private const int _speedDigits = 3;
 
   private readonly InputInfo _inputInfo;
+  private readonly SpeedProvider<GravitySpeed> _gravitySpeedProvider = new();
   private readonly SpeedProvider<float> _slopeFactorSpeedProvider;
   private readonly SpeedProvider<Vector2> _groundToAirSpeedProvider;
 
@@ -14,10 +15,12 @@ public class PlayerSpeedManager
 
   public PlayerSpeedManager(
     InputInfo inputInfo,
+    SpeedProvider<GravitySpeed> gravitySpeedProvider,
     SpeedProvider<float> slopeFactorSpeedProvider,
     SpeedProvider<Vector2> groundToAirSpeedProvider)
   {
     _inputInfo = inputInfo;
+    _gravitySpeedProvider = gravitySpeedProvider;
     _slopeFactorSpeedProvider = slopeFactorSpeedProvider;
     _groundToAirSpeedProvider = groundToAirSpeedProvider;
   }
@@ -81,14 +84,9 @@ public class PlayerSpeedManager
 
   private void SetSpeed_Airborne_Gravity(PlayerSpeedInput input)
   {
-    if (!input.GravityEnabled)
-    {
-      return;
-    }
+    var gravitySpeed = _gravitySpeedProvider.FirstTriggeredOrDefault();
 
-    SpeedY -= SpeedY > 0
-      ? input.GravityUpSpeed
-      : input.GravityDownSpeed;
+    SpeedY -= SpeedY > 0 ? gravitySpeed.Up : gravitySpeed.Down;
 
     if (SpeedY < -input.MaxFallSpeed)
     {
