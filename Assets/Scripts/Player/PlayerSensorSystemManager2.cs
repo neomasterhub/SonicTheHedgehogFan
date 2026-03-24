324e8cc7cacf8812736c0fe78b977e4e94a3a022
Neomaster X <neomasterx@outlook.com>
2026-03-24 22:32:03 +0300

    #195 ref: layerSensorSystemManager2._sensorDefs

--- Assets/Scripts/Player/PlayerSensorSystemManager2.cs
+++ Assets/Scripts/Player/PlayerSensorSystemManager2.cs
@@ -5,9 +5,11 @@ public class PlayerSensorSystemManager2<TSizeMode>
   where TSizeMode : struct
 {
   private readonly Dictionary<SensorId, Sensor> _sensors = new();
+  private readonly Dictionary<TSizeMode, Dictionary<GroundSide, Dictionary<SensorId, SensorDef>>> _sensorDefs;
 
-  public PlayerSensorSystemManager2()
+  public PlayerSensorSystemManager2(Dictionary<TSizeMode, Dictionary<GroundSide, Dictionary<SensorId, SensorDef>>> sensorDefs)
   {
+    _sensorDefs = sensorDefs;
     _sensors.Add(SensorId.A, new(Color.limeGreen));
     _sensors.Add(SensorId.B, new(Color.green));
   }
