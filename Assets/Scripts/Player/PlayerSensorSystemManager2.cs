48b9c43ff16091bfd30afeba3f807d8ad6d90c0d
Neomaster X <neomasterx@outlook.com>
2026-03-24 21:38:12 +0300

    #195 ref: PlayerSensorSystemManager2: _sensors

---
+++ Assets/Scripts/Player/PlayerSensorSystemManager2.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSensorSystemManager2
+{
+  private readonly Dictionary<SensorId, Sensor> _sensors = new();
+
+  public PlayerSensorSystemManager2()
+  {
+    _sensors.Add(SensorId.A, new(Color.limeGreen));
+    _sensors.Add(SensorId.B, new(Color.green));
+  }
+}
