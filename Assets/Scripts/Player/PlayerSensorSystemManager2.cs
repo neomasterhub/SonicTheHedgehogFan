d7f73eb9a6e6fd96aa19d6c7c52f2e11520cb66c
Neomaster X <neomasterx@outlook.com>
2026-03-24 22:37:52 +0300

    #195 ref: layerSensorSystemManager2.Update()

--- Assets/Scripts/Player/PlayerSensorSystemManager2.cs
+++ Assets/Scripts/Player/PlayerSensorSystemManager2.cs
@@ -7,10 +7,24 @@ public class PlayerSensorSystemManager2<TSizeMode>
   private readonly Dictionary<SensorId, Sensor> _sensors = new();
   private readonly Dictionary<TSizeMode, Dictionary<GroundSide, Dictionary<SensorId, SensorDef>>> _sensorDefs;
 
+  private PlayerSensorSystemInput<TSizeMode> _input;
+
   public PlayerSensorSystemManager2(Dictionary<TSizeMode, Dictionary<GroundSide, Dictionary<SensorId, SensorDef>>> sensorDefs)
   {
     _sensorDefs = sensorDefs;
     _sensors.Add(SensorId.A, new(Color.limeGreen));
     _sensors.Add(SensorId.B, new(Color.green));
+    _sensors.Add(SensorId.E, new(Color.yellow));
+    _sensors.Add(SensorId.F, new(Color.orange));
+  }
+
+  public void Update(PlayerSensorSystemInput<TSizeMode> input)
+  {
+    _input = input;
+
+    foreach (var (key, value) in _sensorDefs[_input.SizeMode][_input.GroundSide])
+    {
+      _sensors[key].Update(_input.Parent, value);
+    }
   }
 }
