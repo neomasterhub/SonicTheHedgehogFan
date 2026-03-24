7440f3b23cc4ea0f70a72a8b5cd85f4ecd47bbf0
Neomaster X <neomasterx@outlook.com>
2026-03-24 12:26:43 +0300

    #195 PlayerSensorSystemInput with sensors settings

--- Assets/Scripts/Player/PlayerSensorSystemInput.cs
+++ Assets/Scripts/Player/PlayerSensorSystemInput.cs
@@ -2,52 +2,30 @@ using UnityEngine;
 
 public readonly struct PlayerSensorSystemInput
 {
+  public readonly bool HorizontalDirection;
   public readonly Vector2 Parent;
   public readonly SizeMode SizeMode;
   public readonly GroundSide GroundSide;
   public readonly LayerMask GroundLayer;
-  public readonly float ABSensorLength;
-  public readonly float CDSensorLength;
-  public readonly float EFSensorLength;
-  public readonly float ReversedABSensorLength;
-  public readonly float ReversedCDSensorLength;
-  public readonly float ReversedEFSensorLength;
-  public readonly bool HorizontalDirection;
+  public readonly SensorSettings ASettings;
+  public readonly SensorSettings BSettings;
+  public readonly SensorSettings CSettings;
+  public readonly SensorSettings DSettings;
+  public readonly SensorSettings ESettings;
+  public readonly SensorSettings FSettings;
 
-  public PlayerSensorSystemInput(Vector2 parent, SizeMode sizeMode, GroundSide groundSide, LayerMask groundLayer, float aBSensorLength, float cDSensorLength, float eFSensorLength, float reversedABSensorLength, float reversedCDSensorLength, float reversedEFSensorLength, bool horizontalDirection)
+  public PlayerSensorSystemInput(bool horizontalDirection, Vector2 parent, SizeMode sizeMode, GroundSide groundSide, LayerMask groundLayer, SensorSettings aSettings, SensorSettings bSettings, SensorSettings cSettings, SensorSettings dSettings, SensorSettings eSettings, SensorSettings fSettings)
   {
+    HorizontalDirection = horizontalDirection;
     Parent = parent;
     SizeMode = sizeMode;
     GroundSide = groundSide;
     GroundLayer = groundLayer;
-    ABSensorLength = aBSensorLength;
-    CDSensorLength = cDSensorLength;
-    EFSensorLength = eFSensorLength;
-    ReversedABSensorLength = reversedABSensorLength;
-    ReversedCDSensorLength = reversedCDSensorLength;
-    ReversedEFSensorLength = reversedEFSensorLength;
-    HorizontalDirection = horizontalDirection;
-  }
-
-  public float GetSensorLength(SensorId id)
-  {
-    return id switch
-    {
-      SensorId.A or SensorId.B => ABSensorLength,
-      SensorId.C or SensorId.D => CDSensorLength,
-      SensorId.E or SensorId.F => EFSensorLength,
-      _ => throw id.ArgumentOutOfRangeException(),
-    };
-  }
-
-  public float GetReversedSensorLength(SensorId id)
-  {
-    return id switch
-    {
-      SensorId.A or SensorId.B => ReversedABSensorLength,
-      SensorId.C or SensorId.D => ReversedCDSensorLength,
-      SensorId.E or SensorId.F => ReversedEFSensorLength,
-      _ => throw id.ArgumentOutOfRangeException(),
-    };
+    ASettings = aSettings;
+    BSettings = bSettings;
+    CSettings = cSettings;
+    DSettings = dSettings;
+    ESettings = eSettings;
+    FSettings = fSettings;
   }
 }
