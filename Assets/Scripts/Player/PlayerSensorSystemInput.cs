b12175675d462cc047b1b38c99e892c039cc38fe
Neomaster X <neomasterx@outlook.com>
2026-03-24 22:41:55 +0300

    #195 ref: PlayerSensorSystemInput<TSizeMode>

--- Assets/Scripts/Player/PlayerSensorSystemInput.cs
+++ Assets/Scripts/Player/PlayerSensorSystemInput.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 
-public readonly struct PlayerSensorSystemInput
+public readonly struct PlayerSensorSystemInput<TSizeMode>
+  where TSizeMode : struct
 {
   public readonly bool HorizontalDirection;
   public readonly Vector2 Parent;
-  public readonly SizeMode SizeMode;
+  public readonly TSizeMode SizeMode;
   public readonly GroundSide GroundSide;
   public readonly LayerMask GroundLayer;
   public readonly SensorSettings ASettings;
@@ -13,21 +14,6 @@ public readonly struct PlayerSensorSystemInput
   public readonly SensorSettings DSettings;
   public readonly SensorSettings ESettings;
   public readonly SensorSettings FSettings;
-
-  public PlayerSensorSystemInput(bool horizontalDirection, Vector2 parent, SizeMode sizeMode, GroundSide groundSide, LayerMask groundLayer, SensorSettings aSettings, SensorSettings bSettings, SensorSettings cSettings, SensorSettings dSettings, SensorSettings eSettings, SensorSettings fSettings)
-  {
-    HorizontalDirection = horizontalDirection;
-    Parent = parent;
-    SizeMode = sizeMode;
-    GroundSide = groundSide;
-    GroundLayer = groundLayer;
-    ASettings = aSettings;
-    BSettings = bSettings;
-    CSettings = cSettings;
-    DSettings = dSettings;
-    ESettings = eSettings;
-    FSettings = fSettings;
-  }
 
   public SensorSettings this[SensorId id]
   {
