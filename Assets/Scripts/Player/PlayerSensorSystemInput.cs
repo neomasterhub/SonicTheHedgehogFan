c7b2c880ba0e7419b5166ee1a9bf11ea0f03947d
Neomaster X <neomasterx@outlook.com>
2026-03-24 22:52:01 +0300

    #195 ref: PlayerSensorSystemInput<TSizeMode>.ParentPosition

--- Assets/Scripts/Player/PlayerSensorSystemInput.cs
+++ Assets/Scripts/Player/PlayerSensorSystemInput.cs
@@ -4,7 +4,7 @@ public readonly struct PlayerSensorSystemInput<TSizeMode>
   where TSizeMode : struct
 {
   public readonly bool HorizontalDirection;
-  public readonly Vector2 Parent;
+  public readonly Vector2 ParentPosition;
   public readonly TSizeMode SizeMode;
   public readonly GroundSide GroundSide;
   public readonly LayerMask GroundLayer;
