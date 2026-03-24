a8606371332048f750aafd0b9b640181973856e5
Neomaster X <neomasterx@outlook.com>
2026-03-24 23:06:49 +0300

    #195 ref: PlayerSensorSystemManager2 -> _sensorRaySettings

--- Assets/Scripts/Player/PlayerSensorSystemManager2.cs
+++ Assets/Scripts/Player/PlayerSensorSystemManager2.cs
@@ -5,13 +5,18 @@ public class PlayerSensorSystemManager2<TSizeMode>
   where TSizeMode : struct
 {
   private readonly Dictionary<SensorId, Sensor> _sensors = new();
+  private readonly Dictionary<SensorId, Dictionary<char, SensorRaySettings>> _sensorRaySettings;
   private readonly Dictionary<TSizeMode, Dictionary<GroundSide, Dictionary<SensorId, SensorDef>>> _sensorDefs;
 
   private PlayerSensorSystemInput<TSizeMode> _input;
 
-  public PlayerSensorSystemManager2(Dictionary<TSizeMode, Dictionary<GroundSide, Dictionary<SensorId, SensorDef>>> sensorDefs)
+  public PlayerSensorSystemManager2(
+    Dictionary<SensorId, Dictionary<char, SensorRaySettings>> sensorRaySettings,
+    Dictionary<TSizeMode, Dictionary<GroundSide, Dictionary<SensorId, SensorDef>>> sensorDefs)
   {
     _sensorDefs = sensorDefs;
+    _sensorRaySettings = sensorRaySettings;
+
     _sensors.Add(SensorId.A, new(Color.limeGreen));
     _sensors.Add(SensorId.B, new(Color.green));
     _sensors.Add(SensorId.E, new(Color.yellow));
