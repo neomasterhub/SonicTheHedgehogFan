7aa54f4bf5e2a6809d56d3037d481554203e462a
Neomaster X <neomasterx@outlook.com>
2026-03-25 10:13:50 +0300

    #195 SensorRay.SensorRay()

--- Assets/Scripts/Sensor/SensorRay.cs
+++ Assets/Scripts/Sensor/SensorRay.cs
@@ -6,4 +6,13 @@ public class SensorRay
   public float Length { get; set; }
   public Color Color { get; set; }
   public Vector2 Direction { get; set; }
+
+  public void Draw(Vector2 source)
+  {
+    if (Enabled)
+    {
+      Gizmos.color = Color;
+      Gizmos.DrawLine(source, Direction * Length);
+    }
+  }
 }
