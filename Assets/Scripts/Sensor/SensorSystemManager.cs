2e73348e2ebf3c7d6603efe09f61b3b02428a657
Neomaster X <neomasterx@outlook.com>
2026-03-25 10:45:57 +0300

    #195 SensorSystemManager big and small sensor groups

--- Assets/Scripts/Sensor/SensorSystemManager.cs
+++ Assets/Scripts/Sensor/SensorSystemManager.cs
@@ -2,10 +2,14 @@ public class SensorSystemManager
 {
   private SensorSystemInput _input;
 
-  public SensorGroup UpSensorGroup { get; set; }
-  public SensorGroup DownSensorGroup { get; set; }
-  public SensorGroup LeftSensorGroup { get; set; }
-  public SensorGroup RightSensorGroup { get; set; }
+  public SensorGroup BigUpSensorGroup { get; set; }
+  public SensorGroup BigDownSensorGroup { get; set; }
+  public SensorGroup BigLeftSensorGroup { get; set; }
+  public SensorGroup BigRightSensorGroup { get; set; }
+  public SensorGroup SmallUpSensorGroup { get; set; }
+  public SensorGroup SmallDownSensorGroup { get; set; }
+  public SensorGroup SmallLeftSensorGroup { get; set; }
+  public SensorGroup SmallRightSensorGroup { get; set; }
   public SensorGroup CurrentSensorGroup { get; private set; }
 
   public void Update(SensorSystemInput input)
