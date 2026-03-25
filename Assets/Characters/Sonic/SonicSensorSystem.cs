8f42911ab61cdffbea1bcb6b7bf1bf329a4cfb66
Neomaster X <neomasterx@outlook.com>
2026-03-25 20:06:41 +0300

    #195 REF: Update SonicSensorSystem props access

--- Assets/Characters/Sonic/SonicSensorSystem.cs
+++ Assets/Characters/Sonic/SonicSensorSystem.cs
@@ -3,30 +3,39 @@ using static SonicConsts;
 
 public class SonicSensorSystem
 {
+  private readonly SonicSensorGroup _bigUpSensorGroup;
+  private readonly SonicSensorGroup _bigDownSensorGroup;
+  private readonly SonicSensorGroup _bigLeftSensorGroup;
+  private readonly SonicSensorGroup _bigRightSensorGroup;
+  private readonly SonicSensorGroup _smallUpSensorGroup;
+  private readonly SonicSensorGroup _smallDownSensorGroup;
+  private readonly SonicSensorGroup _smallLeftSensorGroup;
+  private readonly SonicSensorGroup _smallRightSensorGroup;
+
   public SonicSensorSystem()
   {
-    BigDownSensorGroup = new()
+    _bigDownSensorGroup = new()
     {
       A = new(Color.limeGreen, new(-Sizes.Big.HRadius, -Sizes.Big.VRadius), Vector2.up, Vector2.down, Vector2.left),
       B = new(Color.limeGreen, new(Sizes.Big.HRadius, -Sizes.Big.VRadius), Vector2.up, Vector2.down, Vector2.right),
       C = new(Color.green, new(-Sizes.Big.HRadius, Sizes.Big.VRadius), Vector2.up, Vector2.down, Vector2.left),
       D = new(Color.green, new(Sizes.Big.HRadius, Sizes.Big.VRadius), Vector2.up, Vector2.down, Vector2.right),
     };
-    BigRightSensorGroup = new()
+    _bigRightSensorGroup = new()
     {
       A = new(Color.limeGreen, new(Sizes.Big.VRadius, -Sizes.Big.HRadius), Vector2.left, Vector2.right, Vector2.down),
       B = new(Color.limeGreen, new(Sizes.Big.VRadius, Sizes.Big.HRadius), Vector2.left, Vector2.right, Vector2.up),
       C = new(Color.green, new(-Sizes.Big.VRadius, -Sizes.Big.HRadius), Vector2.left, Vector2.right, Vector2.down),
       D = new(Color.green, new(-Sizes.Big.VRadius, Sizes.Big.HRadius), Vector2.left, Vector2.right, Vector2.up),
     };
-    BigUpSensorGroup = new()
+    _bigUpSensorGroup = new()
     {
       C = new(Color.limeGreen, new(-Sizes.Big.HRadius, -Sizes.Big.VRadius), Vector2.down, Vector2.up, Vector2.right),
       D = new(Color.limeGreen, new(Sizes.Big.HRadius, -Sizes.Big.VRadius), Vector2.down, Vector2.up, Vector2.left),
       A = new(Color.green, new(-Sizes.Big.HRadius, Sizes.Big.VRadius), Vector2.down, Vector2.up, Vector2.right),
       B = new(Color.green, new(Sizes.Big.HRadius, Sizes.Big.VRadius), Vector2.down, Vector2.up, Vector2.left),
     };
-    BigRightSensorGroup = new()
+    _bigRightSensorGroup = new()
     {
       C = new(Color.limeGreen, new(Sizes.Big.VRadius, -Sizes.Big.HRadius), Vector2.right, Vector2.left, Vector2.up),
       D = new(Color.limeGreen, new(Sizes.Big.VRadius, Sizes.Big.HRadius), Vector2.right, Vector2.left, Vector2.down),
@@ -35,14 +44,6 @@ public class SonicSensorSystem
     };
   }
 
-  public SonicSensorGroup BigUpSensorGroup { get; set; }
-  public SonicSensorGroup BigDownSensorGroup { get; set; }
-  public SonicSensorGroup BigLeftSensorGroup { get; set; }
-  public SonicSensorGroup BigRightSensorGroup { get; set; }
-  public SonicSensorGroup SmallUpSensorGroup { get; set; }
-  public SonicSensorGroup SmallDownSensorGroup { get; set; }
-  public SonicSensorGroup SmallLeftSensorGroup { get; set; }
-  public SonicSensorGroup SmallRightSensorGroup { get; set; }
   public SonicSensorGroup CurrentSensorGroup { get; private set; }
 
   public void SetCurrentSensorGroup(SonicSizeMode sizeMode, GroundSide groundSide)
@@ -51,18 +52,18 @@ public class SonicSensorSystem
     {
       SonicSizeMode.Big => groundSide switch
       {
-        GroundSide.Up => BigUpSensorGroup,
-        GroundSide.Down => BigDownSensorGroup,
-        GroundSide.Left => BigLeftSensorGroup,
-        GroundSide.Right => BigRightSensorGroup,
+        GroundSide.Up => _bigUpSensorGroup,
+        GroundSide.Down => _bigDownSensorGroup,
+        GroundSide.Left => _bigLeftSensorGroup,
+        GroundSide.Right => _bigRightSensorGroup,
         _ => throw groundSide.ArgumentOutOfRangeException(),
       },
       SonicSizeMode.Small => groundSide switch
       {
-        GroundSide.Up => SmallUpSensorGroup,
-        GroundSide.Down => SmallDownSensorGroup,
-        GroundSide.Left => SmallLeftSensorGroup,
-        GroundSide.Right => SmallRightSensorGroup,
+        GroundSide.Up => _smallUpSensorGroup,
+        GroundSide.Down => _smallDownSensorGroup,
+        GroundSide.Left => _smallLeftSensorGroup,
+        GroundSide.Right => _smallRightSensorGroup,
         _ => throw groundSide.ArgumentOutOfRangeException(),
       },
       _ => throw sizeMode.ArgumentOutOfRangeException(),
