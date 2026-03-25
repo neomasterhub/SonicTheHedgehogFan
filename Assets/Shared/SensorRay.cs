8ff8eed5b165200bae74b3c1b484a27b046505de
Neomaster X <neomasterx@outlook.com>
2026-03-25 19:31:49 +0300

    #195 REF: SensorRay ctors

--- Assets/Shared/SensorRay.cs
+++ Assets/Shared/SensorRay.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class SensorRay
 {
+  public SensorRay()
+  {
+  }
+
+  public SensorRay(Color color, Vector2 direction)
+  {
+    Color = color;
+    Direction = direction;
+  }
+
   public bool Enabled { get; set; }
   public float Length { get; set; }
   public Color Color { get; set; }
