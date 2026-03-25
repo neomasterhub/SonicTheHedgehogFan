fd03c3f580d76eba30e2eaacde3576bc304d684b
Neomaster X <neomasterx@outlook.com>
2026-03-25 20:55:33 +0300

    #195 REF: SonicSensorGroup.SetSensorPositions()

--- Assets/Characters/Sonic/SonicSensorGroup.cs
+++ Assets/Characters/Sonic/SonicSensorGroup.cs
@@ -1,9 +1,19 @@
+using UnityEngine;
+
 public class SonicSensorGroup
 {
   public UDFSensor A { get; set; }
   public UDFSensor B { get; set; }
   public UDFSensor C { get; set; }
   public UDFSensor D { get; set; }
+
+  public void SetSensorPositions(Vector2 parentPosition)
+  {
+    A.SetPosition(parentPosition);
+    B.SetPosition(parentPosition);
+    C.SetPosition(parentPosition);
+    D.SetPosition(parentPosition);
+  }
 
   public void Draw()
   {
