127b36d8a11582f4e31e05444c82f2621ef888f7
Neomaster X <neomasterx@outlook.com>
2026-03-25 10:18:20 +0300

    #195 UDFSensor.Draw()

--- Assets/Scripts/Sensor/UDFSensor.cs
+++ Assets/Scripts/Sensor/UDFSensor.cs
@@ -3,10 +3,29 @@ using UnityEngine;
 public class UDFSensor
 {
   public bool Enabled { get; set; }
+  public float Radius { get; set; }
   public Color EnabledColor { get; set; }
   public Color? DisabledColor { get; set; }
   public Vector2 Position { get; set; }
   public SensorRay UpRay { get; set; }
   public SensorRay DownRay { get; set; }
   public SensorRay FrontRay { get; set; }
+
+  public void Draw()
+  {
+    if (Enabled)
+    {
+      UpRay.Draw(Position);
+      DownRay.Draw(Position);
+      FrontRay.Draw(Position);
+
+      Gizmos.color = EnabledColor;
+      Gizmos.DrawSphere(Position, Radius);
+    }
+    else if (DisabledColor.HasValue)
+    {
+      Gizmos.color = DisabledColor.Value;
+      Gizmos.DrawSphere(Position, Radius);
+    }
+  }
 }
