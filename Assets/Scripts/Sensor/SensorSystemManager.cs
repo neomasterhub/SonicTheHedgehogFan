f4ff32e0a98aaf364ea4d3648dc4840d5be09673
Neomaster X <neomasterx@outlook.com>
2026-03-25 10:44:54 +0300

    #195 SensorSystemManager.Update() draft

--- Assets/Scripts/Sensor/SensorSystemManager.cs
+++ Assets/Scripts/Sensor/SensorSystemManager.cs
@@ -1,7 +1,15 @@
 public class SensorSystemManager
 {
+  private SensorSystemInput _input;
+
   public SensorGroup UpSensorGroup { get; set; }
   public SensorGroup DownSensorGroup { get; set; }
   public SensorGroup LeftSensorGroup { get; set; }
   public SensorGroup RightSensorGroup { get; set; }
+  public SensorGroup CurrentSensorGroup { get; private set; }
+
+  public void Update(SensorSystemInput input)
+  {
+    _input = input;
+  }
 }
