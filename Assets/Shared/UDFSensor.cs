748431e2ce4653b506c464485c030ec86ca4456f
Neomaster X <neomasterx@outlook.com>
2026-03-26 02:02:23 +0300

    #195 REF: UDFSensor defaults

--- Assets/Shared/UDFSensor.cs
+++ Assets/Shared/UDFSensor.cs
@@ -16,10 +16,10 @@ public class UDFSensor
     FrontRay = new(enabledColor, frontRayPosition);
   }
 
-  public bool Enabled { get; set; }
-  public float Radius { get; set; }
-  public Color EnabledColor { get; set; }
-  public Color? DisabledColor { get; set; }
+  public bool Enabled { get; set; } = true;
+  public float Radius { get; set; } = 0.03f;
+  public Color EnabledColor { get; set; } = Color.red;
+  public Color? DisabledColor { get; set; } = Color.darkRed;
   public Vector2 Position { get; private set; }
   public Vector2 LocalPosition { get; private set; }
   public Vector2 ParentPosition { get; private set; }
