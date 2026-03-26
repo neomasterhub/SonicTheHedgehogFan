3b81b27d6e340d2cc90bc014899aa94af162f426
Neomaster X <neomasterx@outlook.com>
2026-03-26 12:27:55 +0300

    #195 REF: SonicController v2: top and bottom UDF sensor ray lengths

--- Assets/Characters/Sonic/SonicController.cs
+++ Assets/Characters/Sonic/SonicController.cs
@@ -4,11 +4,9 @@ public class SonicController : MonoBehaviour
 {
   private readonly SonicSensorSystem _sensorSystem = new();
 
-  public SonicController()
-  {
-    // For drawing
-    _sensorSystem.SetCurrentSensorGroup(default, default);
-  }
+  [Header("Sensors")]
+  public Vector3 TopUDFLengths = new(0.3f, 0.3f, 0.5f);
+  public Vector3 BottomUDFLengths = new(0.3f, 0.1f, 0.5f);
 
   private void OnDrawGizmos()
   {
