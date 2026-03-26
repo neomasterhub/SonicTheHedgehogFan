410153ebeacceea20efd5990cb51f0db369e2956
Neomaster X <neomasterx@outlook.com>
2026-03-26 14:07:02 +0300

    #195 REF: SonicSensorSystem: set sensor ray lengths

--- Assets/Characters/Sonic/SonicSensorSystem.cs
+++ Assets/Characters/Sonic/SonicSensorSystem.cs
@@ -12,6 +12,9 @@ public class SonicSensorSystem
   private readonly SonicSensorGroup _smallLeftSensorGroup;
   private readonly SonicSensorGroup _smallRightSensorGroup;
 
+  private Vector3 _topUDFLengths;
+  private Vector3 _bottomUDFLengths;
+
   public SonicSensorSystem(
     SonicSizeMode sizeMode = SonicSizeMode.Big,
     GroundSide groundSide = GroundSide.Down,
@@ -65,7 +68,9 @@ public class SonicSensorSystem
   public void Update(
     SonicSizeMode sizeMode,
     GroundSide groundSide,
-    Vector2 parentPosition)
+    Vector2 parentPosition,
+    Vector3 topUDFLengths,
+    Vector3 bottomUDFLengths)
   {
     if (SizeMode != sizeMode || GroundSide != groundSide)
     {
@@ -75,6 +80,28 @@ public class SonicSensorSystem
     }
 
     ParentPosition = parentPosition;
+
+    if (_bottomUDFLengths != bottomUDFLengths)
+    {
+      _bottomUDFLengths = bottomUDFLengths;
+      CurrentSensorGroup.A.UpRay.Length = bottomUDFLengths.x;
+      CurrentSensorGroup.A.DownRay.Length = bottomUDFLengths.y;
+      CurrentSensorGroup.A.FrontRay.Length = bottomUDFLengths.z;
+      CurrentSensorGroup.B.UpRay.Length = bottomUDFLengths.x;
+      CurrentSensorGroup.B.DownRay.Length = bottomUDFLengths.y;
+      CurrentSensorGroup.B.FrontRay.Length = bottomUDFLengths.z;
+    }
+
+    if (_topUDFLengths != topUDFLengths)
+    {
+      _topUDFLengths = topUDFLengths;
+      CurrentSensorGroup.C.UpRay.Length = topUDFLengths.x;
+      CurrentSensorGroup.C.DownRay.Length = topUDFLengths.y;
+      CurrentSensorGroup.C.FrontRay.Length = topUDFLengths.z;
+      CurrentSensorGroup.D.UpRay.Length = topUDFLengths.x;
+      CurrentSensorGroup.D.DownRay.Length = topUDFLengths.y;
+      CurrentSensorGroup.D.FrontRay.Length = topUDFLengths.z;
+    }
   }
 
   private void SetCurrentSensorGroup()
