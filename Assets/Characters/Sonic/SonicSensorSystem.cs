17f4bceffaf54d2a53cbf2b23a93d4a3fb15ca14
Neomaster X <neomasterx@outlook.com>
2026-03-26 18:40:04 +0300

    #195 REF: SonicSensorSystem: fix big-left group initialization

--- Assets/Characters/Sonic/SonicSensorSystem.cs
+++ Assets/Characters/Sonic/SonicSensorSystem.cs
@@ -46,7 +46,7 @@ public class SonicSensorSystem
       a: new(aColor, new(-Sizes.Big.HRadius, Sizes.Big.VRadius), Vector2.down, Vector2.up, Vector2.right),
       b: new(bColor, new(Sizes.Big.HRadius, Sizes.Big.VRadius), Vector2.down, Vector2.up, Vector2.left),
       parentPosition: parentPosition);
-    _bigRightSensorGroup = new(
+    _bigLeftSensorGroup = new(
       c: new(cColor, new(Sizes.Big.VRadius, -Sizes.Big.HRadius), Vector2.right, Vector2.left, Vector2.up),
       d: new(dColor, new(Sizes.Big.VRadius, Sizes.Big.HRadius), Vector2.right, Vector2.left, Vector2.down),
       a: new(aColor, new(-Sizes.Big.VRadius, -Sizes.Big.HRadius), Vector2.right, Vector2.left, Vector2.up),
