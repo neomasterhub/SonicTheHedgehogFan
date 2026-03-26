4c274a30306006185d8d94547267b0f358187014
Neomaster X <neomasterx@outlook.com>
2026-03-26 11:54:52 +0300

    #195 REF: SonicController v2: OnDrawGizmos()

--- Assets/Characters/Sonic/SonicController.cs
+++ Assets/Characters/Sonic/SonicController.cs
@@ -3,4 +3,15 @@ using UnityEngine;
 public class SonicController : MonoBehaviour
 {
   private readonly SonicSensorSystem _sensorSystem = new();
+
+  public SonicController()
+  {
+    // For drawing
+    _sensorSystem.SetCurrentSensorGroup(default, default);
+  }
+
+  private void OnDrawGizmos()
+  {
+    _sensorSystem.CurrentSensorGroup.Draw();
+  }
 }
