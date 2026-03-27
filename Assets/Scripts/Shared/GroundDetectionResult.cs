74c134a37e9a9478f31c108a8de03a227407d5e6
Neomaster X <neomasterx@outlook.com>
2026-03-27 20:58:02 +0300

    #215 GroundDetectionResult.Update()

--- Assets/Scripts/Shared/GroundDetectionResult.cs
+++ Assets/Scripts/Shared/GroundDetectionResult.cs
@@ -7,4 +7,18 @@ public class GroundDetectionResult
   public float AngleRad { get; private set; }
   public Vector2 Contact { get; private set; }
   public Vector2 Normal { get; private set; }
+
+  public void Update(
+    float distance,
+    float angleDeg,
+    float angleRad,
+    Vector2 contact,
+    Vector2 normal)
+  {
+    Distance = distance;
+    AngleDeg = angleDeg;
+    AngleRad = angleRad;
+    Contact = contact;
+    Normal = normal;
+  }
 }
