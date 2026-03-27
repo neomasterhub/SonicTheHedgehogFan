f13adacce6c2178f340843cf7dd49133c9a691fe
Neomaster X <neomasterx@outlook.com>
2026-03-27 19:43:53 +0300

    #215 SensorRay.Origin

--- Assets/Scripts/Shared/SensorRay.cs
+++ Assets/Scripts/Shared/SensorRay.cs
@@ -6,23 +6,25 @@ public class SensorRay
   {
   }
 
-  public SensorRay(Color color, Vector2 direction)
+  public SensorRay(Color color, Vector2 origin, Vector2 direction)
   {
     Color = color;
+    Origin = origin;
     Direction = direction;
   }
 
   public bool Enabled { get; set; } = true;
   public float Length { get; set; } = 1;
   public Color Color { get; set; } = Color.red;
+  public Vector2 Origin { get; set; }
   public Vector2 Direction { get; set; } = Vector2.right;
 
-  public void Draw(Vector2 origin)
+  public void Draw()
   {
     if (Enabled)
     {
       Gizmos.color = Color;
-      Gizmos.DrawLine(origin, origin + (Direction * Length));
+      Gizmos.DrawLine(Origin, Origin + (Direction * Length));
     }
   }
 }
