5df702afb4253baa8bce1b920e539e3e280b8d21
Neomaster X <neomasterx@outlook.com>
2026-03-28 11:13:23 +0300

    #227 Improve PlayerSpeedContext

--- Assets/Scripts/Player/PlayerSpeedContext.cs
+++ Assets/Scripts/Player/PlayerSpeedContext.cs
@@ -1,40 +1,11 @@
 public class PlayerSpeedContext
 {
-  public readonly float TopSpeed;
-  public readonly float FrictionSpeed;
-  public readonly float AccelerationSpeed;
-  public readonly float DecelerationSpeed;
-
-  public readonly float AirTopSpeed;
-  public readonly float AirAccelerationSpeed;
-  public readonly float MaxFallSpeed;
-
-  public readonly float InputDeadZone;
-  public readonly float SkiddingSpeedDeadZone;
-
-  public PlayerSpeedContext(float topSpeed, float frictionSpeed, float accelerationSpeed, float decelerationSpeed, float airTopSpeed, float airAccelerationSpeed, float maxFallSpeed, float inputDeadZone, float skiddingSpeedDeadZone, bool isGrounded, bool prevIsGrounded, float? groundAngleRad, float? distanceToGround)
-  {
-    TopSpeed = topSpeed;
-    FrictionSpeed = frictionSpeed;
-    AccelerationSpeed = accelerationSpeed;
-    DecelerationSpeed = decelerationSpeed;
-    AirTopSpeed = airTopSpeed;
-    AirAccelerationSpeed = airAccelerationSpeed;
-    MaxFallSpeed = maxFallSpeed;
-    InputDeadZone = inputDeadZone;
-    SkiddingSpeedDeadZone = skiddingSpeedDeadZone;
-    IsGrounded = isGrounded;
-    PrevIsGrounded = prevIsGrounded;
-    GroundAngleRad = groundAngleRad;
-    DistanceToGround = distanceToGround;
-  }
-
   public bool IsGrounded { get; private set; }
   public bool PrevIsGrounded { get; private set; }
   public float? GroundAngleRad { get; private set; }
   public float? DistanceToGround { get; private set; }
 
-  public void UpdateGrounded(bool prevIsGrounded, float groundAngleRad, float distanceToGround)
+  public void SetGrounded(bool prevIsGrounded, float groundAngleRad, float distanceToGround)
   {
     IsGrounded = true;
     PrevIsGrounded = prevIsGrounded;
@@ -42,7 +13,7 @@ public class PlayerSpeedContext
     DistanceToGround = distanceToGround;
   }
 
-  public void UpdateAirborne(bool prevIsGrounded)
+  public void SetAirborne(bool prevIsGrounded)
   {
     IsGrounded = false;
     PrevIsGrounded = prevIsGrounded;
