904f53b3e0c89ce005cfc8081ce3b4200aa7afb3
Neomaster X <neomasterx@outlook.com>
2026-03-28 14:44:24 +0300

    #227 SonicController: GravityEnabled

--- Assets/Scripts/Characters/Sonic/SonicController.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.cs
@@ -33,6 +33,8 @@ public class SonicController : MonoBehaviour
   [Header("Sensors")]
   public Vector3 TopUDFLengths = new(0.3f, 0.3f, 0.5f);
   public Vector3 BottomUDFLengths = new(0.3f, 0.1f, 0.5f);
+  [Header("Physics")]
+  public bool GravityEnabled = true;
 
   public SonicController()
   {
