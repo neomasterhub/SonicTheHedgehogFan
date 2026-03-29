e04076bbab03dbdf8191fa0743bdc40d344b2303
Neomaster X <neomasterx@outlook.com>
2026-03-29 21:30:53 +0300

    #231 AnalyzeEnvironment()

--- Assets/Scripts/Characters/Sonic/SonicController.Pipeline.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Pipeline.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 /// <summary>
 /// Pipeline.
 /// </summary>
@@ -5,14 +7,36 @@ public partial class SonicController
 {
   private void FixedUpdate()
   {
-    Update();
+    AnalyzeEnvironment();
     Output();
   }
 
-  private void Update()
+  private void AnalyzeEnvironment()
   {
     _prevState = _state;
     _prevGroundSide = _groundSide;
     _prevIsGrounded = _isGrounded;
+
+    _timerSystem.Update(Time.deltaTime);
+    _inputSystem.Update(!_postWallDetachInputLock);
+    _sensorSystem.Update(_sizeMode, _groundSide, transform.position, TopUDFLengths, BottomUDFLengths);
+
+    var groundDetectionResult = _sensorSystem.DetectGround(!_spriteRenderer.flipX, _groundLayer);
+    if (groundDetectionResult != null)
+    {
+      AnalyzeEnvironment_Grounded();
+    }
+    else
+    {
+      AnalyzeEnvironment_Airborn();
+    }
+  }
+
+  private void AnalyzeEnvironment_Grounded()
+  {
+  }
+
+  private void AnalyzeEnvironment_Airborn()
+  {
   }
 }
