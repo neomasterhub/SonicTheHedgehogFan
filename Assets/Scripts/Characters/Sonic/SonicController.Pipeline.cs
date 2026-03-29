f525de1d9bfae9e835d93575fca33432f47942c8
Neomaster X <neomasterx@outlook.com>
2026-03-29 21:28:15 +0300

    #231 1

--- Assets/Scripts/Characters/Sonic/SonicController.Pipeline.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Pipeline.cs
@@ -5,6 +5,14 @@ public partial class SonicController
 {
   private void FixedUpdate()
   {
+    Update();
     Output();
+  }
+
+  private void Update()
+  {
+    _prevState = _state;
+    _prevGroundSide = _groundSide;
+    _prevIsGrounded = _isGrounded;
   }
 }
