b6e784052083e3f42d676e98016cf9b26eb5bbbf
Neomaster X <neomasterx@outlook.com>
2026-03-29 20:38:49 +0300

    #231 fix: InitializeSpeedSystemProviders()

--- Assets/Scripts/Characters/Sonic/SonicController.Init.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Init.cs
@@ -22,20 +22,20 @@ public partial class SonicController
 
   private void InitializeSpeedSystemProviders()
   {
-    var gravitySpeed = new GravitySpeed(GravityUpSpeed, GravityDownSpeed);
     var defaultGravitySpeed = new GravitySpeed(0, 0);
+    var gravitySpeed = new GravitySpeed(GravityUpSpeed, GravityDownSpeed);
 
     _gravitySpeedProvider
       .When(() => GravityEnabled && _groundSide == GroundSide.Down, () => gravitySpeed);
 
     _slopeFactorSpeedProvider
       .When(() => _groundSide == GroundSide.Down, () => SlopeFactor * Mathf.Sin(_relativeGroundInfo.AngleRad))
-      .When(() => _groundSide == GroundSide.Left, () => 0)
-      .When(() => _groundSide == GroundSide.Right, () => 0);
+      .When(() => _groundSide == GroundSide.Left, () => _relativeGroundInfo.AngleDeg <= 0 ? SlopeFactor : SlopeFactor * Mathf.Cos(_relativeGroundInfo.AngleRad))
+      .When(() => _groundSide == GroundSide.Right, () => _relativeGroundInfo.AngleDeg >= 0 ? SlopeFactor : SlopeFactor * Mathf.Cos(_relativeGroundInfo.AngleRad));
 
     _groundToAirSpeedProvider
-      .When(() => _prevGroundSide == GroundSide.Right, () => WallToAirSpeedDelta + new Vector2(-_speedSystem.SpeedY, _speedSystem.SpeedX))
-      .When(() => _prevGroundSide == GroundSide.Left, () => WallToAirSpeedDelta + new Vector2(_speedSystem.SpeedY, -_speedSystem.SpeedX));
+      .When(() => _prevGroundSide == GroundSide.Left, () => WallToAirSpeedDelta + new Vector2(_speedSystem.SpeedY, -_speedSystem.SpeedX))
+      .When(() => _prevGroundSide == GroundSide.Right, () => WallToAirSpeedDelta + new Vector2(-_speedSystem.SpeedY, _speedSystem.SpeedX));
 
     _gravitySpeedProvider.DefaultProvider = () => defaultGravitySpeed;
     _groundToAirSpeedProvider.DefaultProvider = () => new(_speedSystem.SpeedX, _speedSystem.SpeedY);
