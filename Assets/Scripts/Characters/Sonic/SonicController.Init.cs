c8bef6a6331820432009cd3625df7c2c0240bb1b
Neomaster X <neomasterx@outlook.com>
2026-03-29 20:01:19 +0300

    #231 Init unity settings

--- Assets/Scripts/Characters/Sonic/SonicController.Init.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Init.cs
@@ -1,6 +1,14 @@
+using UnityEngine;
+using static SharedConsts.ConvertValues;
+
 /// <summary>
 /// Initializations.
 /// </summary>
 public partial class SonicController
 {
+  private void Awake()
+  {
+    Application.targetFrameRate = FramePerSec;
+    Time.fixedDeltaTime = 1f / FramePerSec;
+  }
 }
