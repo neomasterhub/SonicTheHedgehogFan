b1a8cfeb4d3b37df8e975af37e45b558772a1bb3
Neomaster X <neomasterx@outlook.com>
2026-04-05 13:21:10 +0300

    #235 SonicViewContext

--- Assets/Scripts/Characters/Sonic/SonicViewContext.cs
+++ Assets/Scripts/Characters/Sonic/SonicViewContext.cs
@@ -1,3 +1,10 @@
 public readonly struct SonicViewContext
 {
+  public readonly bool IsGrounded;
+  public readonly bool IsSkidding;
+  public readonly bool IsBalancing;
+  public readonly float SpeedX;
+  public readonly SonicState State;
+  public readonly SonicState PrevState;
+  public readonly GroundSide PrevGroundSide;
 }
