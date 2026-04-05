e58eb1bc34ddf0ddd83a911814630ef537913ac6
Neomaster X <neomasterx@outlook.com>
2026-04-05 21:56:09 +0300

    #242 ref: SonicViewContext ctor

--- Assets/Scripts/Characters/Sonic/SonicViewContext.cs
+++ Assets/Scripts/Characters/Sonic/SonicViewContext.cs
@@ -7,4 +7,15 @@ public readonly struct SonicViewContext
   public readonly float GroundSpeed;
   public readonly float GroundAngleDeg;
   public readonly GroundSide PrevGroundSide;
+
+  public SonicViewContext(bool isGrounded, bool isSkidding, bool isBalancing, float speedX, float groundSpeed, float groundAngleDeg, GroundSide prevGroundSide)
+  {
+    IsGrounded = isGrounded;
+    IsSkidding = isSkidding;
+    IsBalancing = isBalancing;
+    SpeedX = speedX;
+    GroundSpeed = groundSpeed;
+    GroundAngleDeg = groundAngleDeg;
+    PrevGroundSide = prevGroundSide;
+  }
 }
