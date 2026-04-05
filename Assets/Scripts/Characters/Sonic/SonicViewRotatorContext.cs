3c535cafca4d924b9c5c3cca996a726b74418ee3
Neomaster X <neomasterx@outlook.com>
2026-04-05 18:44:58 +0300

    #239 SonicViewRotatorContext ctor

--- Assets/Scripts/Characters/Sonic/SonicViewRotatorContext.cs
+++ Assets/Scripts/Characters/Sonic/SonicViewRotatorContext.cs
@@ -1,4 +1,13 @@
 public readonly struct SonicViewRotatorContext
 {
   public readonly bool IsGrounded;
+  public readonly float GroundSpeed;
+  public readonly float GroundAngleDeg;
+
+  public SonicViewRotatorContext(bool isGrounded, float groundSpeed, float groundAngleDeg)
+  {
+    IsGrounded = isGrounded;
+    GroundSpeed = groundSpeed;
+    GroundAngleDeg = groundAngleDeg;
+  }
 }
