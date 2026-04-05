c59a9f7ae48280e0d14b8ee11145a9169b4f9e3c
Neomaster X <neomasterx@outlook.com>
2026-04-05 12:47:51 +0300

    #235 SonicViewSystem.Update(SonicViewContext) draft

--- Assets/Scripts/Characters/Sonic/SonicViewSystem.cs
+++ Assets/Scripts/Characters/Sonic/SonicViewSystem.cs
@@ -6,10 +6,17 @@ public class SonicViewSystem
   private readonly PlayerInputSystem _inputSystem;
   private readonly SpriteRenderer _spriteRenderer;
 
+  private SonicViewContext _context;
+
   public SonicViewSystem(Animator animator, PlayerInputSystem inputSystem, SpriteRenderer spriteRenderer)
   {
     _animator = animator;
     _inputSystem = inputSystem;
     _spriteRenderer = spriteRenderer;
+  }
+
+  public void Update(SonicViewContext context)
+  {
+    _context = context;
   }
 }
