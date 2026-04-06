2e9c19534d871a66338d1161fb292bc8488cb77c
Neomaster X <neomasterx@outlook.com>
2026-04-06 09:49:11 +0300

    #244 GroundInfo: remove side range id

--- Assets/Scripts/Shared/GroundInfo.cs
+++ Assets/Scripts/Shared/GroundInfo.cs
@@ -1,18 +1,16 @@
-using static SharedConsts.Physics.GroundAngleRanges;
+using UnityEngine;
 
 public class GroundInfo
 {
-  private readonly Range _flatRangeDeg = Flat;
-  private readonly Range _slopeRangeDeg = Slope;
-
   public float AngleDeg { get; private set; }
   public float AngleRad { get; private set; }
   public GroundSide Side { get; private set; }
-  public GroundRangeId SideRangeId { get; private set; }
   public float SideAngleDeg { get; private set; }
   public float SideAngleRad { get; private set; }
 
   public void Update(float sideNormalAngleDeg)
   {
+    SideAngleDeg = sideNormalAngleDeg;
+    SideAngleRad = sideNormalAngleDeg * Mathf.Deg2Rad;
   }
 }
