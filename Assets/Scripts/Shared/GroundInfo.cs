4c303ce7a52b36aa30b79423200cf5818a7a7ac6
Neomaster X <neomasterx@outlook.com>
2026-04-06 10:20:01 +0300

    #244 GroundInfo

---
+++ Assets/Scripts/Shared/GroundInfo.cs
@@ -0,0 +1,19 @@
+public readonly struct GroundInfo
+{
+  public static readonly GroundInfo Default = new(0, 0, GroundSide.Down, 0, 0);
+
+  public readonly float AngleDeg;
+  public readonly float AngleRad;
+  public readonly GroundSide Side;
+  public readonly float SideAngleDeg;
+  public readonly float SideAngleRad;
+
+  public GroundInfo(float angleDeg, float angleRad, GroundSide side, float sideAngleDeg, float sideAngleRad)
+  {
+    AngleDeg = angleDeg;
+    AngleRad = angleRad;
+    Side = side;
+    SideAngleDeg = sideAngleDeg;
+    SideAngleRad = sideAngleRad;
+  }
+}
