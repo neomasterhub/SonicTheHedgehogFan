f00219978f8314220f2b59aaf7f1408ae7c3b6c7
Neomaster X <neomasterx@outlook.com>
2026-04-06 09:36:18 +0300

    #244 GroundInfo.Update()

--- Assets/Scripts/Shared/GroundInfo.cs
+++ Assets/Scripts/Shared/GroundInfo.cs
@@ -1,5 +1,10 @@
+using static SharedConsts.Physics.GroundAngleRanges;
+
 public class GroundInfo
 {
+  private readonly Range _flatRangeDeg = Flat;
+  private readonly Range _slopeRangeDeg = Slope;
+
   public float AngleDeg { get; private set; }
   public float AngleRad { get; private set; }
   public GroundSide Side { get; private set; }
