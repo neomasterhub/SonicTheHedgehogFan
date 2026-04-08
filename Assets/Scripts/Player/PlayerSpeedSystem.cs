896963fc04b3254b344601e1ec4f51c41149a158
Neomaster X <neomasterx@outlook.com>
2026-04-08 04:37:20 +0300

    #265 remove PlayerSpeedSystem.IsIdle

--- Assets/Scripts/Player/PlayerSpeedSystem.cs
+++ Assets/Scripts/Player/PlayerSpeedSystem.cs
@@ -30,7 +30,6 @@ public class PlayerSpeedSystem
     _groundToAirSpeedProvider = groundToAirSpeedProvider;
   }
 
-  public bool IsIdle { get; private set; }
   public bool IsSkidding { get; private set; }
   public float SpeedX { get; private set; }
   public float SpeedY { get; private set; }
