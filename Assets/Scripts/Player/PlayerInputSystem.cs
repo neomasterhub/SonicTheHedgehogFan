383520616924765ccec9a1f6ebcac921c7b6d47d
Neomaster X <neomasterx@outlook.com>
2026-04-08 15:45:14 +0300

    #271 ref: remove PlayerInputSystem history

--- Assets/Scripts/Player/PlayerInputSystem.cs
+++ Assets/Scripts/Player/PlayerInputSystem.cs
@@ -3,23 +3,15 @@ using UnityEngine;
 
 public class PlayerInputSystem
 {
-  private readonly int _inputHistoryLimit;
-  private readonly InputState[] _inputHistory;
   private readonly Func<Vector2> _dPadInputSrc;
   private readonly Func<PlayerInput> _buttonInputSrc;
 
-  private int _inputHistoryIndex = 0;
-
   public PlayerInputSystem(
     Func<Vector2> dPadSrc,
-    Func<PlayerInput> buttonInputSrc,
-    int inputHistoryLimit = 10)
+    Func<PlayerInput> buttonInputSrc)
   {
     _dPadInputSrc = dPadSrc;
     _buttonInputSrc = buttonInputSrc;
-
-    _inputHistoryLimit = inputHistoryLimit;
-    _inputHistory = new InputState[inputHistoryLimit];
   }
 
   public Vector2 DPadInput { get; private set; } = default;
@@ -29,10 +21,5 @@ public class PlayerInputSystem
   {
     DPadInput = _dPadInputSrc();
     ButtonInput = _buttonInputSrc();
-
-    Debug.Log($"ButtonInput: {ButtonInput}");
-
-    _inputHistory[_inputHistoryIndex] = new(DPadInput, ButtonInput);
-    _inputHistoryIndex = (_inputHistoryIndex + 1) % _inputHistoryLimit;
   }
 }
