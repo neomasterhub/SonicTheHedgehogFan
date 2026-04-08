d24e92ed729b6015f46d57b071fafde64a7b0398
Neomaster X <neomasterx@outlook.com>
2026-04-08 09:10:47 +0300

    #256 A B C button vector

--- Assets/Scripts/Player/PlayerInputSystem.cs
+++ Assets/Scripts/Player/PlayerInputSystem.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 public class PlayerInputSystem
 {
   private readonly Func<Vector2> _dPadSrc;
+  private readonly Func<Vector3> _abcSrc;
 
-  public PlayerInputSystem(Func<Vector2> dPadSrc)
+  public PlayerInputSystem(Func<Vector2> dPadSrc, Func<Vector3> abcSrc)
   {
     _dPadSrc = dPadSrc;
+    _abcSrc = abcSrc;
   }
 
   public Vector2 DPad { get; private set; }
+  public Vector3 ABC { get; private set; }
 
   public void Update()
   {
     DPad = _dPadSrc();
+    ABC = _abcSrc();
   }
 }
