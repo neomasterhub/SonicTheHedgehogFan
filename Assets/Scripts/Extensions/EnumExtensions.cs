279078278b57e7464600a87e9c5479cc0c62bc77
Neomaster X <neomasterx@outlook.com>
2026-04-09 09:15:23 +0300

    #271 PlayerInput.HasAll(), .HasAny()

--- Assets/Scripts/Extensions/EnumExtensions.cs
+++ Assets/Scripts/Extensions/EnumExtensions.cs
@@ -1,5 +1,15 @@
 public static class EnumExtensions
 {
+  public static bool HasAll(this PlayerInput source, PlayerInput target)
+  {
+    return (source & target) == target;
+  }
+
+  public static bool HasAny(this PlayerInput source, PlayerInput target)
+  {
+    return (source & target) != 0;
+  }
+
   public static PlayerInput SetFlag(this PlayerInput source, PlayerInput target, bool enabled)
   {
     return enabled ? source | target : source & ~target;
