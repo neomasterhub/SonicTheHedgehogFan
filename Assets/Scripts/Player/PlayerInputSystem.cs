511e7d279785e847ef91afda884838c1a9b840ac
Neomaster X <neomasterx@outlook.com>
2026-04-09 08:25:12 +0300

    #271 ref: Remove DPad

--- Assets/Scripts/Player/PlayerInputSystem.cs
+++ Assets/Scripts/Player/PlayerInputSystem.cs
@@ -1,28 +1,20 @@
 using System;
-using UnityEngine;
 
 public class PlayerInputSystem
 {
-  private readonly Func<Vector2> _dPadInputSrc;
   private readonly Func<PlayerInput> _inputSrc;
 
   private PlayerInput _prevInput;
 
-  public PlayerInputSystem(
-    Func<Vector2> dPadSrc,
-    Func<PlayerInput> inputSrc)
+  public PlayerInputSystem(Func<PlayerInput> inputSrc)
   {
-    _dPadInputSrc = dPadSrc;
     _inputSrc = inputSrc;
   }
 
-  public Vector2 DPadInput { get; private set; } = default;
   public PlayerInput Input { get; private set; }
 
   public void Update()
   {
-    DPadInput = _dPadInputSrc();
-
     _prevInput = Input;
     Input = _inputSrc();
   }
