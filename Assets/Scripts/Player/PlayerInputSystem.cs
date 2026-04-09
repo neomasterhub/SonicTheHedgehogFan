eab4133be4067839aebc34784a44135a2a6918e0
Neomaster X <neomasterx@outlook.com>
2026-04-09 09:03:16 +0300

    #271 PlayerInputSystem.Held, Pressed

--- Assets/Scripts/Player/PlayerInputSystem.cs
+++ Assets/Scripts/Player/PlayerInputSystem.cs
@@ -4,20 +4,22 @@ public class PlayerInputSystem
 {
   private readonly Func<PlayerInput> _inputSrc;
 
-  private PlayerInput _previous;
+  private PlayerInput _prev;
 
   public PlayerInputSystem(Func<PlayerInput> inputSrc)
   {
     _inputSrc = inputSrc;
   }
 
-  public PlayerInput Current { get; private set; }
+  public PlayerInput Held { get; private set; }
   public PlayerInput Pressed { get; private set; }
+  public PlayerInput Released { get; private set; }
 
   public void Update()
   {
-    _previous = Current;
-    Current = _inputSrc();
-    Pressed = Current & ~_previous;
+    _prev = Held;
+    Held = _inputSrc();
+    Pressed = Held & ~_prev;
+    Released = _prev & ~Held;
   }
 }
