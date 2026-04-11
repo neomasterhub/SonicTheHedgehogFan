ba9cc74f3fa4efcb019a5bfe6cc79c8779b95c0f
Neomaster X <neomasterx@outlook.com>
2026-04-11 22:27:39 +0300

    #250 Sound.Play(), Stop()

--- Assets/Scripts/Shared/Sound.cs
+++ Assets/Scripts/Shared/Sound.cs
@@ -1,11 +1,32 @@
+using System;
 using UnityEngine;
 
 public class Sound
 {
   private readonly AudioSource _audioSource;
+  private readonly Func<bool> _playCondition;
+  private readonly Func<bool> _stopCondition;
 
-  public Sound(AudioSource audioSource)
+  public Sound(AudioSource audioSource, Func<bool> playCondition, Func<bool> stopCondition)
   {
     _audioSource = audioSource;
+    _playCondition = playCondition;
+    _stopCondition = stopCondition;
+  }
+
+  public void Play()
+  {
+    if (_playCondition())
+    {
+      _audioSource.Play();
+    }
+  }
+
+  public void Stop()
+  {
+    if (_stopCondition())
+    {
+      _audioSource.Stop();
+    }
   }
 }
