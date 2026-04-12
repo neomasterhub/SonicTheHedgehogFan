74cd01986a5766f8ae8dc362a3f0db65cf777545
Neomaster X <neomasterx@outlook.com>
2026-04-12 04:54:51 +0300

    #258 Update SonicSensorSystem

--- Assets/Scripts/Characters/Sonic/SonicSensorSystem.cs
+++ Assets/Scripts/Characters/Sonic/SonicSensorSystem.cs
@@ -27,30 +27,35 @@ public class SonicSensorSystem
     var bColor = Color.green;
     var cColor = Color.softYellow;
     var dColor = Color.yellow;
+    var oColor = Color.white;
 
     _bigDownSensorGroup = new(
       a: new(aColor, new(-Big.HRadius, -Big.VRadius), Vector2.up, Vector2.down, Vector2.left),
       b: new(bColor, new(Big.HRadius, -Big.VRadius), Vector2.up, Vector2.down, Vector2.right),
       c: new(cColor, new(-Big.HRadius, Big.VRadius), Vector2.up, Vector2.down, Vector2.left),
       d: new(dColor, new(Big.HRadius, Big.VRadius), Vector2.up, Vector2.down, Vector2.right),
+      o: new(oColor, Vector2.zero, Vector2.down),
       parentPosition: parentPosition);
     _bigRightSensorGroup = new(
       a: new(aColor, new(Big.VRadius, -Big.HRadius), Vector2.left, Vector2.right, Vector2.down),
       b: new(bColor, new(Big.VRadius, Big.HRadius), Vector2.left, Vector2.right, Vector2.up),
       c: new(cColor, new(-Big.VRadius, -Big.HRadius), Vector2.left, Vector2.right, Vector2.down),
       d: new(dColor, new(-Big.VRadius, Big.HRadius), Vector2.left, Vector2.right, Vector2.up),
+      o: new(oColor, Vector2.zero, Vector2.right),
       parentPosition: parentPosition);
     _bigUpSensorGroup = new(
       c: new(cColor, new(-Big.HRadius, -Big.VRadius), Vector2.down, Vector2.up, Vector2.right),
       d: new(dColor, new(Big.HRadius, -Big.VRadius), Vector2.down, Vector2.up, Vector2.left),
       a: new(aColor, new(-Big.HRadius, Big.VRadius), Vector2.down, Vector2.up, Vector2.right),
       b: new(bColor, new(Big.HRadius, Big.VRadius), Vector2.down, Vector2.up, Vector2.left),
+      o: new(oColor, Vector2.zero, Vector2.up),
       parentPosition: parentPosition);
     _bigLeftSensorGroup = new(
       c: new(cColor, new(Big.VRadius, -Big.HRadius), Vector2.right, Vector2.left, Vector2.up),
       d: new(dColor, new(Big.VRadius, Big.HRadius), Vector2.right, Vector2.left, Vector2.down),
       a: new(aColor, new(-Big.VRadius, -Big.HRadius), Vector2.right, Vector2.left, Vector2.up),
       b: new(bColor, new(-Big.VRadius, Big.HRadius), Vector2.right, Vector2.left, Vector2.down),
+      o: new(oColor, Vector2.zero, Vector2.left),
       parentPosition: parentPosition);
 
     SetCurrentSensorGroup();
