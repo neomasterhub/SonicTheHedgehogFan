7ea94dca7d0546e321c025722e550f8156082ba5
Neomaster X <neomasterx@outlook.com>
2026-04-15 11:26:24 +0300

    #283 Output current rotator

--- Assets/Scripts/Characters/Sonic/SonicController.Diagnostics.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Diagnostics.cs
@@ -17,6 +17,7 @@ public partial class SonicController
     _info.AppendLine($"GC {_groundInfoSystem.Current}");
     _info.AppendLine($"GS {_speedSystem.SlopeFactorSpeed * 10000,5:0;-0;0} {_speedSystem.GroundSpeed * 10000,5:0;-0;0}");
     _info.AppendLine($"SP {_speedSystem.SpeedX * 10000,5:0;-0;0} {_speedSystem.SpeedY * 10000,5:0;-0;0}");
+    _info.AppendLine($"ROT {_viewSystem.Rotator,10}");
 
     _infoText.SetText(_info);
   }
