5ec20cea21173dc11cd4a52a161001fb6aaef102
Neomaster X <neomasterx@outlook.com>
2026-04-18 20:54:12 +0300

    #301 Add flag _isPostWallDetached

--- Assets/Scripts/Characters/Sonic/SonicController.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.cs
@@ -26,6 +26,7 @@ public partial class SonicController : MonoBehaviour
 
   private bool _isGrounded;
   private bool _isBalancing;
+  private bool _isPostWallDetached;
   private bool _prevIsGrounded;
   private bool _postWallDetachInputLock;
   private bool _triggeredGroundSensorSide;
