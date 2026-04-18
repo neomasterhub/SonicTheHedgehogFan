6e294a32fbba5fabfa24b7a0663b97991dc1a5ed
Neomaster X <neomasterx@outlook.com>
2026-04-18 10:03:06 +0300

    #297 SonicSensorRayLengths: ==, != operators

--- Assets/Scripts/Characters/Sonic/SonicSensorRayLengths.cs
+++ Assets/Scripts/Characters/Sonic/SonicSensorRayLengths.cs
@@ -14,8 +14,33 @@ public readonly struct SonicSensorRayLengths : IEquatable<SonicSensorRayLengths>
     BottomUDF = bottomUDF;
   }
 
+  public static bool operator ==(SonicSensorRayLengths left, SonicSensorRayLengths right)
+  {
+    return left.Equals(right);
+  }
+
+  public static bool operator !=(SonicSensorRayLengths left, SonicSensorRayLengths right)
+  {
+    return !left.Equals(right);
+  }
+
   public bool Equals(SonicSensorRayLengths other)
   {
     return O == other.O && TopUDF == other.TopUDF && BottomUDF == other.BottomUDF;
+  }
+
+  public override bool Equals(object obj)
+  {
+    return obj is SonicSensorRayLengths other && Equals(other);
+  }
+
+  public override int GetHashCode()
+  {
+    var hashCode = default(HashCode);
+    hashCode.Add(O);
+    hashCode.Add(TopUDF);
+    hashCode.Add(BottomUDF);
+
+    return hashCode.ToHashCode();
   }
 }
