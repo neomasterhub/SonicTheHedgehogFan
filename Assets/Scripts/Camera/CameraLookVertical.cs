4851b3c7b0016a43e253170f63e9ad7ef52c8f70
Neomaster X <neomasterx@outlook.com>
2026-04-20 21:56:11 +0300

    #313 SonicController : ILookVerticalDirection

--- Assets/Scripts/Camera/CameraLookVertical.cs
+++ Assets/Scripts/Camera/CameraLookVertical.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 [RequireComponent(typeof(CinemachinePositionComposer))]
 public class CameraLookVertical : MonoBehaviour
 {
-  private CinemachinePositionComposer _ccamPositionComposer;
-
   private void Awake()
   {
-    _ccamPositionComposer = GetComponent<CinemachinePositionComposer>();
+  }
+
+  private void Update()
+  {
   }
 }
