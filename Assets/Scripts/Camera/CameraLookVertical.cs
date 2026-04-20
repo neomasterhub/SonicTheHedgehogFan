56e16e69755acf0426f2239d512a3ac2cc0a9193
Neomaster X <neomasterx@outlook.com>
2026-04-20 09:10:39 +0300

    #313 _ccamPositionComposer

--- Assets/Scripts/Camera/CameraLookVertical.cs
+++ Assets/Scripts/Camera/CameraLookVertical.cs
@@ -1,5 +1,13 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
+[RequireComponent(typeof(CinemachinePositionComposer))]
 public class CameraLookVertical : MonoBehaviour
 {
+  private CinemachinePositionComposer _ccamPositionComposer;
+
+  private void Awake()
+  {
+    _ccamPositionComposer = GetComponent<CinemachinePositionComposer>();
+  }
 }
