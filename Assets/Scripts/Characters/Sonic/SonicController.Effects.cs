4205b010ace5277c5eee9e9551744b4b50c585b8
Neomaster X <neomasterx@outlook.com>
2026-04-23 21:45:08 +0300

    #321 ref: rename: CreateEffect_*()

--- Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
@@ -5,10 +5,10 @@ public partial class SonicController
 {
   private void SetEffectPipeline()
   {
-    _effects.AddStep(CreateStep_Rolling_Exit());
+    _effects.AddStep(CreateEffect_Rolling_Exit());
   }
 
-  private PipelineStep CreateStep_Rolling_Exit()
+  private PipelineStep CreateEffect_Rolling_Exit()
   {
     return PipelineStepBuilder.Create()
       .WithDisplayName("Rolling/Exit")
