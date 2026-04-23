665669ac42b8ae1a5d1d6a5ae207128f69191de6
Neomaster X <neomasterx@outlook.com>
2026-04-23 16:52:41 +0300

    #321 Exit Rolling

--- Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
@@ -1,115 +1,25 @@
-using UnityEngine;
-using static SonicConsts.Physics;
-
 /// <summary>
 /// Effects.
 /// </summary>
 public partial class SonicController
 {
-  private void ApplyEffects()
+  public SonicController()
   {
-    if (_isGrounded)
-    {
-      ApplyEffects_Grounded();
-    }
-  }
-
-  private void ApplyEffects_Grounded()
-  {
-    if (_speedSystem.GroundSpeed == 0)
-    {
-      ApplyEffects_Grounded_Static();
-    }
-    else
-    {
-      ApplyEffects_Grounded_Moving();
-    }
-  }
-
-  private void ApplyEffects_Grounded_Static()
-  {
-    if (_isDownGrounded)
-    {
-      if (_isRolling)
+    var eExitRolling = PipelineStepBuilder.Create()
+      .WithDisplayName("Exit Rolling")
+      .WithCondition(() =>
+        _isDownGrounded
+        && _speedSystem.GroundSpeed == 0
+        && _isRolling)
+      .WithAction(() =>
       {
         _isRolling = false;
         _sizeMode = SonicSizeMode.Big;
-      }
 
-      if (!_isBalancing)
-      {
-        // Curling up / Looking up
-        if (_inputSystem.Released == PlayerInput.Down)
-        {
-          _sizeMode = SonicSizeMode.Big;
-          _isCurlingUp = false;
-          return;
-        }
+        return PipelineStepResult.Continue;
+      })
+      .Build();
 
-        if (_inputSystem.Held.HasAny(PlayerInput.Down))
-        {
-          _sizeMode = SonicSizeMode.Small;
-          _isCurlingUp = true;
-          return;
-        }
-
-        if (_inputSystem.Released == PlayerInput.Up)
-        {
-          _isLookingUp = false;
-          return;
-        }
-
-        if (_inputSystem.Held.HasAny(PlayerInput.Up))
-        {
-          _isLookingUp = true;
-          return;
-        }
-      }
-    }
-  }
-
-  private void ApplyEffects_Grounded_Moving()
-  {
-    ApplyEffects_Grounded_Moving_ExitStatic();
-
-    // Start input unlock timer
-    if (_isFallingOffWall)
-    {
-      _isFallingOffWall = false;
-      _timerSystem.StartIfNotRunning(_inputUnlockTimer);
-      return;
-    }
-
-    // Rolling
-    if (_isDownGrounded
-      && !_isBalancing
-      && _inputSystem.Pressed.HasAny(PlayerInput.Down))
-    {
-      _isRolling = true;
-      _sizeMode = SonicSizeMode.Small;
-      return;
-    }
-
-    // Wall detach
-    if (!_isDownGrounded
-      && Mathf.Abs(_speedSystem.GroundSpeed) < DecelerationSpeed)
-    {
-      _isFallingOffWall = true;
-      _postWallDetachInputLock = true;
-      _postWallDetachPositionOffset = true;
-      AnalyzeEnvironment_Airborn();
-      return;
-    }
-  }
-
-  private void ApplyEffects_Grounded_Moving_ExitStatic()
-  {
-    if (_isCurlingUp)
-    {
-      _sizeMode = SonicSizeMode.Big;
-    }
-
-    _isCurlingUp = false;
-    _isLookingUp = false;
+    _effects.AddStep(eExitRolling);
   }
 }
