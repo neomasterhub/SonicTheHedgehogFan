367983912ce253360ae8f375b34215606a6824bb
Neomaster X <neomasterx@outlook.com>
2026-04-23 15:11:17 +0300

    #321 ApplyEffects_Grounded_Moving_ExitStatic()

--- Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
@@ -3,4 +3,14 @@
 /// </summary>
 public partial class SonicController
 {
+  private void ApplyEffects_Grounded_Moving_ExitStatic()
+  {
+    if (_isCurlingUp)
+    {
+      _sizeMode = SonicSizeMode.Big;
+    }
+
+    _isCurlingUp = false;
+    _isLookingUp = false;
+  }
 }
