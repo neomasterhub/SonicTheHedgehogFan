9e2b1565f58d22e2bb41e7cfb3122c5564c29bd4
Neomaster X <neomasterx@outlook.com>
2026-04-23 15:14:18 +0300

    #321 ApplyEffects_Grounded() v2

--- Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
@@ -3,6 +3,22 @@
 /// </summary>
 public partial class SonicController
 {
+  private void ApplyEffects_Grounded()
+  {
+    if (_speedSystem.GroundSpeed == 0)
+    {
+      ApplyEffects_Grounded_Static();
+    }
+    else
+    {
+      ApplyEffects_Grounded_Moving();
+    }
+  }
+
+  private void ApplyEffects_Grounded_Static()
+  {
+  }
+
   private void ApplyEffects_Grounded_Moving()
   {
     ApplyEffects_Grounded_Moving_ExitStatic();
