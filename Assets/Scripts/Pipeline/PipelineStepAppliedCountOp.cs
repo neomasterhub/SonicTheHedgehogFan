d6352c9afb09ed3943764990614be1a8ba8e4f91
Neomaster X <neomasterx@outlook.com>
2026-04-25 04:55:37 +0300

    #336 PipelineStepAppliedCountOp

---
+++ Assets/Scripts/Pipeline/PipelineStepAppliedCountOp.cs
@@ -0,0 +1,5 @@
+public enum PipelineStepAppliedCountOp
+{
+  Equal,
+  GreaterThan,
+}
--- Assets/Scripts/Pipeline/PipelineStepInfo.cs
+++ Assets/Scripts/Pipeline/PipelineStepInfo.cs
@@ -3,4 +3,5 @@ public struct PipelineStepInfo
   public int AppliedCount;
   public string DisplayName;
   public PipelineStepResult Result;
+  public PipelineStepAppliedCountOp AppliedCountOp;
 }
