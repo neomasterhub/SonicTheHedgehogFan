5e4a438e591c4f7c44f0ff2cfccbb9356843da1b
Neomaster X <neomasterx@outlook.com>
2026-04-26 13:57:54 +0300

    #332 SonicSpeedContext.IsJumping

--- Assets/Scripts/Characters/Sonic/SonicSpeedContext.cs
+++ Assets/Scripts/Characters/Sonic/SonicSpeedContext.cs
@@ -1,6 +1,7 @@
 public readonly struct SonicSpeedContext
 {
   public readonly bool IsRolling;
+  public readonly bool IsJumping;
   public readonly bool IsGrounded;
   public readonly bool PrevIsGrounded;
   public readonly float? GroundAngleRad;
@@ -8,9 +9,10 @@ public readonly struct SonicSpeedContext
   public readonly float? DistanceToLeftWall;
   public readonly float? DistanceToRightWall;
 
-  private SonicSpeedContext(bool isRolling, bool isGrounded, bool prevIsGrounded, float? groundAngleRad, float? distanceToGround, float? distanceToLeftWall, float? distanceToRightWall)
+  private SonicSpeedContext(bool isRolling, bool isJumping, bool isGrounded, bool prevIsGrounded, float? groundAngleRad, float? distanceToGround, float? distanceToLeftWall, float? distanceToRightWall)
   {
     IsRolling = isRolling;
+    IsJumping = isJumping;
     IsGrounded = isGrounded;
     PrevIsGrounded = prevIsGrounded;
     GroundAngleRad = groundAngleRad;
@@ -19,13 +21,13 @@ public readonly struct SonicSpeedContext
     DistanceToRightWall = distanceToRightWall;
   }
 
-  public static SonicSpeedContext GetGrounded(bool isRolling, bool prevIsGrounded, float groundAngleRad, float distanceToGround, float? distanceToLeftWall, float? distanceToRightWall)
+  public static SonicSpeedContext GetGrounded(bool isRolling, bool isJumping, bool prevIsGrounded, float groundAngleRad, float distanceToGround, float? distanceToLeftWall, float? distanceToRightWall)
   {
-    return new(isRolling, true, prevIsGrounded, groundAngleRad, distanceToGround, distanceToLeftWall, distanceToRightWall);
+    return new(isRolling, isJumping, true, prevIsGrounded, groundAngleRad, distanceToGround, distanceToLeftWall, distanceToRightWall);
   }
 
-  public static SonicSpeedContext GetAirborne(bool isRolling, bool prevIsGrounded, float? distanceToLeftWall, float? distanceToRightWall)
+  public static SonicSpeedContext GetAirborne(bool isRolling, bool isJumping, bool prevIsGrounded, float? distanceToLeftWall, float? distanceToRightWall)
   {
-    return new(isRolling, false, prevIsGrounded, null, null, distanceToLeftWall, distanceToRightWall);
+    return new(isRolling, isJumping, false, prevIsGrounded, null, null, distanceToLeftWall, distanceToRightWall);
   }
 }
