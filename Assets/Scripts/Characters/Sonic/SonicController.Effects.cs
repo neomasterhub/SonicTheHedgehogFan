aa72e4b4b0fa4aecaa251f20abe6702cfff6c620
Neomaster X <neomasterx@outlook.com>
2026-04-30 21:42:16 +0300

    #390 fix: double jump

--- Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.Effects.cs
@@ -23,8 +23,8 @@ public partial class SonicController
     return PipelineStepBuilder.Create()
       .WithDisplayName("Jumping/Exit")
       .WithCondition(() =>
-        !_isGrounded
-        && _speedSystem.SpeedY <= 0)
+        (_isGrounded && !_prevIsGrounded)
+        || (!_isGrounded && _speedSystem.SpeedY <= 0))
       .WithAction(() =>
       {
         _isJumping = false;
