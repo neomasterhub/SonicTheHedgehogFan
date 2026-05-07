6bb5ae0f447f192d9f269d8d621c09264cd4da01
Neomaster X <neomasterx@outlook.com>
2026-05-07 21:19:31 +0300

    #421 Increase ring sorting order after collected

--- Assets/Scripts/Objects/Ring/RingController.cs
+++ Assets/Scripts/Objects/Ring/RingController.cs
@@ -3,11 +3,13 @@ using static SharedConsts.Animator.Parameters;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class RingController : MonoBehaviour
 {
   private bool _collected;
   private Animator _animator;
   private BoxCollider2D _collider;
+  private SpriteRenderer _spriteRenderer;
 
   [SerializeField]
   private BoxCollider2D _playerCollider;
@@ -16,6 +18,7 @@ public class RingController : MonoBehaviour
   {
     _animator = GetComponent<Animator>();
     _collider = GetComponent<BoxCollider2D>();
+    _spriteRenderer = GetComponent<SpriteRenderer>();
   }
 
   private void FixedUpdate()
@@ -29,6 +32,7 @@ public class RingController : MonoBehaviour
     {
       _collected = true;
       _animator.SetTrigger(Collected);
+      _spriteRenderer.sortingOrder = 2;
     }
   }
 }
