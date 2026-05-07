b622af0ecf87ceafeb21eec2bc1c58dce2347248
Neomaster X <neomasterx@outlook.com>
2026-05-07 23:03:36 +0300

    #421 remove Collector.PrevCount

--- Assets/Scripts/Shared/Collector.cs
+++ Assets/Scripts/Shared/Collector.cs
@@ -1,17 +1,14 @@
 public class Collector : ICollector
 {
-  public int PrevCount { get; private set; }
   public int Count { get; private set; }
 
   public void Add(int value = 1)
   {
-    PrevCount = Count;
     Count += value;
   }
 
   public void Clear()
   {
-    PrevCount = Count;
     Count = 0;
   }
 }
