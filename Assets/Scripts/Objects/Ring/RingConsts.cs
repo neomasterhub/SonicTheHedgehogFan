6a6e2a53c1556fd6b1b9770625a99d10165e063e
Neomaster X <neomasterx@outlook.com>
2026-05-08 20:30:03 +0300

    #437 Ring sensor ray lengths

--- Assets/Scripts/Objects/Ring/RingConsts.cs
+++ Assets/Scripts/Objects/Ring/RingConsts.cs
@@ -2,7 +2,8 @@ public static class RingConsts
 {
   public static class Physics
   {
-    public const float SensorRayLength = 0.23f;
+    public const float InnerSensorRayLength = 0.3f;
+    public const float OuterSensorRayLength = 0.15f;
     public static readonly RingPhysicsModeConfig NormalConfig = new(0.005f, 0.8f, 0.02f);
   }
 }
