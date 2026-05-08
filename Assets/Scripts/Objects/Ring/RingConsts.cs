b78e289747ef07164dec7f0da3e32cd72f01c915
Neomaster X <neomasterx@outlook.com>
2026-05-08 21:08:46 +0300

    #437 Improve SensorY

--- Assets/Scripts/Objects/Ring/RingConsts.cs
+++ Assets/Scripts/Objects/Ring/RingConsts.cs
@@ -2,7 +2,7 @@ public static class RingConsts
 {
   public static class Physics
   {
-    public const float SensorY = 0.08f;
+    public const float SensorY = 0.075f;
     public const float InnerSensorRayLength = 0.2f;
     public const float OuterSensorRayLength = 0.1f;
     public static readonly RingPhysicsModeConfig NormalConfig = new(0.005f, 0.8f, 0.02f);
