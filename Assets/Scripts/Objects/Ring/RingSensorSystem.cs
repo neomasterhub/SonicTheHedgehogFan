75ee03a24d85d63d50f7072c017bf99e19675e6f
Neomaster X <neomasterx@outlook.com>
2026-05-08 21:04:33 +0300

    #437 fix: RingSensorSystem.DetectGround(): specify vertical relation

--- Assets/Scripts/Objects/Ring/RingSensorSystem.cs
+++ Assets/Scripts/Objects/Ring/RingSensorSystem.cs
@@ -26,17 +26,18 @@ public class RingSensorSystem
 
   public GroundDetectionResult? DetectGround(LayerMask groundLayer)
   {
-    var dir = _a.DownRay.Direction;
     var hit = _a.DownRay.Cast(groundLayer);
-
-    if (hit == null)
+    if (hit != null)
     {
-      dir = _a.UpRay.Direction;
-      hit = _a.UpRay.Cast(groundLayer);
+      return new(false, hit.Value, _a.DownRay.Direction, VerticalRelation.Above);
     }
 
-    return hit.HasValue
-      ? new(false, hit.Value, dir)
-      : null;
+    hit = _a.UpRay.Cast(groundLayer);
+    if (hit != null)
+    {
+      return new(false, hit.Value, _a.UpRay.Direction, VerticalRelation.Below);
+    }
+
+    return null;
   }
 }
