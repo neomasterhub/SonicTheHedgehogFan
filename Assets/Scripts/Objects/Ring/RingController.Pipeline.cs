b375606e17bdf72533b073815cdfd5d1124d0c7e
Neomaster X <neomasterx@outlook.com>
2026-05-09 22:54:45 +0300

    #440 Ring controller partial classes

--- Assets/Scripts/Objects/Ring/RingController.Pipeline.cs
+++ Assets/Scripts/Objects/Ring/RingController.Pipeline.cs
@@ -3,53 +3,11 @@ using static RingConsts.UI;
 using static SharedConsts.Physics;
 using AnimatorParameters = SharedConsts.Animator.Parameters;
 
-[RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(BoxCollider2D))]
-[RequireComponent(typeof(SpriteRenderer))]
-public class RingController : MonoBehaviour
+/// <summary>
+/// Pipeline.
+/// </summary>
+public partial class RingController : MonoBehaviour
 {
-  private readonly RingConfigs _configs;
-  private readonly RingSpeedSystem _speedSystem;
-  private readonly RingSensorSystem _sensorSystem;
-
-  private bool _collected;
-  private bool _isGrounded;
-  private Animator _animator;
-  private BoxCollider2D _collider;
-  private BoxCollider2D _playerCollider;
-  private GroundDetectionResult _lastGroundDetectionResult;
-  private ICollector _playerRings;
-  private RingSpeedContext _speedContext;
-  private SpriteRenderer _spriteRenderer;
-
-  [SerializeField]
-  private bool _gravityEnabled;
-  [SerializeField]
-  private GameObject _player;
-  [SerializeField]
-  private PhysicsMode _physicsMode;
-
-  public RingController()
-  {
-    _configs = new(_physicsMode);
-    _speedSystem = new(_configs);
-    _sensorSystem = new();
-  }
-
-  private void OnDrawGizmos()
-  {
-    _sensorSystem.Draw();
-  }
-
-  private void Awake()
-  {
-    _animator = GetComponent<Animator>();
-    _collider = GetComponent<BoxCollider2D>();
-    _spriteRenderer = GetComponent<SpriteRenderer>();
-    _playerCollider = _player.GetComponent<BoxCollider2D>();
-    _playerRings = _player.GetComponent<IRingCollector>().Rings;
-  }
-
   private void FixedUpdate()
   {
     if (_collected)
