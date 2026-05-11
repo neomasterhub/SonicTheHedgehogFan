4adb988ca8cd50503535219bbcddde4f7215d767
Neomaster X <neomasterx@outlook.com>
2026-05-11 11:58:44 +0300

    #446 Update()

--- Assets/Scripts/Shared/Blinker.cs
+++ Assets/Scripts/Shared/Blinker.cs
@@ -5,33 +5,70 @@ public class Blinker
   private readonly SpriteRenderer _spriteRenderer;
 
   private bool _dimmed;
-  private float _alpha;
+  private bool _isRunning;
   private float _time;
   private float _timer;
   private float _interval;
+  private Color _dimmedColor;
+  private Color _sourceColor;
 
   public Blinker(SpriteRenderer spriteRenderer)
   {
     _spriteRenderer = spriteRenderer;
   }
 
+  public bool IsRunning => _isRunning;
+
   public void Run(float alpha, float timer, float interval)
   {
-    _dimmed = true;
-    _alpha = alpha;
+    _isRunning = true;
 
     _time = 0;
     _timer = timer;
     _interval = interval;
 
+    _sourceColor = _spriteRenderer.color;
+    _dimmedColor = _sourceColor;
+    _dimmedColor.a = alpha;
+
     ToggleAlpha();
+  }
+
+  public void Update(float deltaTime)
+  {
+    if (!_isRunning)
+    {
+      return;
+    }
+
+    if (_timer <= 0)
+    {
+      _time = 0;
+      _timer = 0;
+      _interval = 0;
+      _isRunning = false;
+
+      if (_dimmed)
+      {
+        ToggleAlpha();
+      }
+
+      return;
+    }
+
+    _time += deltaTime;
+    _timer -= deltaTime;
+
+    if (_time >= _interval)
+    {
+      _time = 0;
+      ToggleAlpha();
+    }
   }
 
   private void ToggleAlpha()
   {
-    var color = _spriteRenderer.color;
-    color.a = _dimmed ? _alpha : 1;
-    _spriteRenderer.color = color;
     _dimmed = !_dimmed;
+    _spriteRenderer.color = _dimmed ? _dimmedColor : _sourceColor;
   }
 }
