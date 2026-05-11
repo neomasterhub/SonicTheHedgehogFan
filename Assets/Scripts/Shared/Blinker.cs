b39f93900b381f73a9de4b0b5e38f4c76d4c0972
Neomaster X <neomasterx@outlook.com>
2026-05-11 11:30:41 +0300

    #446 Run()

--- Assets/Scripts/Shared/Blinker.cs
+++ Assets/Scripts/Shared/Blinker.cs
@@ -4,8 +4,24 @@ public class Blinker
 {
   private readonly SpriteRenderer _spriteRenderer;
 
+  private bool _dimmed;
+  private float _alpha;
+  private float _time;
+  private float _timer;
+  private float _interval;
+
   public Blinker(SpriteRenderer spriteRenderer)
   {
     _spriteRenderer = spriteRenderer;
+  }
+
+  public void Run(float alpha, float timer, float interval)
+  {
+    _dimmed = true;
+    _alpha = alpha;
+
+    _time = 0;
+    _timer = timer;
+    _interval = interval;
   }
 }
