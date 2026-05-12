c15c7b1044cf8065415c1ab719216e7f26c8b8d2
Neomaster X <neomasterx@outlook.com>
2026-05-12 13:31:49 +0300

    #465 EnemyController.Init

---
+++ Assets/Scripts/Characters/Enemy/EnemyController.Init.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+/// <summary>
+/// Init.
+/// </summary>
+public partial class EnemyController
+{
+  private void Awake()
+  {
+    _collider = GetComponent<BoxCollider2D>();
+    _spriteRenderer = GetComponent<SpriteRenderer>();
+
+    if (_player != null)
+    {
+      _playerCollider = _player.GetComponent<BoxCollider2D>();
+    }
+  }
+
+  public void Initialize(GameObject player)
+  {
+    _player = player;
+    _playerCollider = _player.GetComponent<BoxCollider2D>();
+
+    _initialized = true;
+  }
+}
