b272d27781df341005a657fcf53e59b225a4f1f6
Neomaster X <neomasterx@outlook.com>
2026-05-12 13:26:16 +0300

    #465 EnemyController

---
+++ Assets/Scripts/Characters/Enemy/EnemyController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/// <summary>
+/// Data.
+/// </summary>
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public partial class EnemyController : MonoBehaviour
+{
+  private BoxCollider2D _collider;
+  private BoxCollider2D _playerCollider;
+  private SpriteRenderer _spriteRenderer;
+
+  [SerializeField]
+  private bool _initialized;
+  [SerializeField]
+  private GameObject _player;
+}
