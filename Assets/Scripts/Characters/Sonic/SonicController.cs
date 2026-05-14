103fe1fca1550ecd214f7bf0f373a449aa6972f5
Neomaster X <neomasterx@outlook.com>
2026-05-14 11:26:29 +0300

    #462 SonicController : ICameraTarget

--- Assets/Scripts/Characters/Sonic/SonicController.cs
+++ Assets/Scripts/Characters/Sonic/SonicController.cs
@@ -10,9 +10,10 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public partial class SonicController
   : MonoBehaviour,
+  ICameraTarget,
+  IEnemy,
   ILookVerticalDirectionProvider,
-  IRingCollector,
-  IEnemy
+  IRingCollector
 {
   private readonly ConditionalValueProvider<float> _slopeSpeedProvider;
   private readonly ConditionalValueProvider<float> _gravitySpeedProvider;
@@ -104,8 +105,18 @@ public partial class SonicController
   [InspectorLabel("Ring")]
   private GameObject _ringPrefab;
 
-  public bool CanCollectRing { get; private set; }
-  public ICollector Rings { get; }
+  // Camera target
+  public bool IsDead => _isDead;
+  public bool IsDying => _isDying;
+
+  // Enemy
+  public bool IsHit { get; set; }
+  public bool IsHurt { get; set; }
+  public bool IsImmortal { get; set; }
+  public bool IsAttacking { get; set; }
+  public GameObject LastHitSource { get; set; }
+
+  // Look vertical direction provider
   public VerticalDirection LookVerticalDirection
   {
     get
@@ -124,9 +135,7 @@ public partial class SonicController
     }
   }
 
-  public bool IsHit { get; set; }
-  public bool IsHurt { get; set; }
-  public bool IsImmortal { get; set; }
-  public bool IsAttacking { get; set; }
-  public GameObject LastHitSource { get; set; }
+  // Ring collector
+  public bool CanCollectRing { get; private set; }
+  public ICollector Rings { get; }
 }
