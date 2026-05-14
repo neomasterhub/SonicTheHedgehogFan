867a0a001ff63b18a9ecb2e8644156f5442a3275
Neomaster X <neomasterx@outlook.com>
2026-05-14 11:18:06 +0300

    #462 ApplyMovement()

--- Assets/Scripts/Camera/CameraController.Pipeline.cs
+++ Assets/Scripts/Camera/CameraController.Pipeline.cs
@@ -7,5 +7,11 @@ public partial class CameraController : MonoBehaviour
 {
   private void FixedUpdate()
   {
+    ApplyMovement();
+  }
+
+  private void ApplyMovement()
+  {
+    _cm.enabled = !_target.IsDying;
   }
 }
