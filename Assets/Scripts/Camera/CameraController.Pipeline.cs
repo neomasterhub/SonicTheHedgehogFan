715c4337ff8ed7472ea2faee0391a6cf04477e87
Neomaster X <neomasterx@outlook.com>
2026-05-15 12:17:40 +0300

    #485 remove ApplyMovement()

--- Assets/Scripts/Camera/CameraController.Pipeline.cs
+++ Assets/Scripts/Camera/CameraController.Pipeline.cs
@@ -8,16 +8,10 @@ public partial class CameraController : MonoBehaviour
   private void FixedUpdate()
   {
     ApplyEffects();
-    ApplyMovement();
   }
 
   private void ApplyEffects()
   {
     _effects.Run(false);
-  }
-
-  private void ApplyMovement()
-  {
-    _cm.enabled = !_target.IsDying;
   }
 }
