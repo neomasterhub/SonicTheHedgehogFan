0ccf7626b10e9b831e2b4a8322c948d160c5024c
Neomaster X <neomasterx@outlook.com>
2026-05-15 12:38:06 +0300

    #485 "Fading out" effect

--- Assets/Scripts/Camera/CameraController.Effects.cs
+++ Assets/Scripts/Camera/CameraController.Effects.cs
@@ -10,6 +10,7 @@ public partial class CameraController : MonoBehaviour
   {
     _effects.AddStep(CreateEffect_RemoveTrackingTarget());
     _effects.AddStep(CreateEffect_FadeOut());
+    _effects.AddStep(CreateEffect_FadingOut());
   }
 
   private PipelineStep CreateEffect_RemoveTrackingTarget()
@@ -39,6 +40,30 @@ public partial class CameraController : MonoBehaviour
         _isFadingOut = true;
         _overlayPanelObj.SetActive(true);
         _overlayPanelImage.color = TransparentBlack;
+
+        return PipelineStepResult.Continue;
+      })
+      .Build();
+  }
+
+  private PipelineStep CreateEffect_FadingOut()
+  {
+    return PipelineStepBuilder.Create()
+      .WithDisplayName("Fading out")
+      .WithCondition(() =>
+        _isFadingOut)
+      .WithAction(() =>
+      {
+        var color = _overlayPanelImage.color;
+        color.a += 0.01f;
+        _overlayPanelImage.color = color;
+
+        if (color.a >= 1)
+        {
+          color.a = 1;
+          _isFadingOut = false;
+          _isFadedOut = true;
+        }
 
         return PipelineStepResult.Break;
       })
--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -9,6 +9,7 @@ public partial class CameraController : MonoBehaviour
 {
   private readonly Pipeline _effects;
 
+  private bool _isFadedOut;
   private bool _isFadingOut;
   private CinemachineCamera _cm;
   private ICameraTarget _target;
