83b535f63804154dc8e7af4290391f59fdc26aa9
Neomaster X <neomasterx@outlook.com>
2026-05-15 12:21:37 +0300

    #485 Camera "Disable following" effect

--- Assets/Scripts/Camera/CameraController.Effects.cs
+++ Assets/Scripts/Camera/CameraController.Effects.cs
@@ -7,7 +7,23 @@ public partial class CameraController : MonoBehaviour
 {
   private void SetEffectPipeline()
   {
+    _effects.AddStep(CreateEffect_DisableFollowing());
     _effects.AddStep(CreateEffect_FadeOut());
+  }
+
+  private PipelineStep CreateEffect_DisableFollowing()
+  {
+    return PipelineStepBuilder.Create()
+      .WithDisplayName("Disable following")
+      .WithCondition(() =>
+        _target.IsDead)
+      .WithAction(() =>
+      {
+        _cm.Target.TrackingTarget = null;
+
+        return PipelineStepResult.Continue;
+      })
+      .Build();
   }
 
   private PipelineStep CreateEffect_FadeOut()
