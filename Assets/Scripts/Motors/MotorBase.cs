e22069c1e70b689ea9e711c33b119cffe88704de
Neomaster X <neomasterx@outlook.com>
2026-05-17 23:25:14 +0300

    #499 MotorBase : IMotor

---
+++ Assets/Scripts/Motors/MotorBase.cs
@@ -0,0 +1,18 @@
+public abstract class MotorBase : IMotor
+{
+  public MotorState State { get; protected set; }
+  public float SpeedX { get; protected set; }
+  public float SpeedY { get; protected set; }
+
+  public void Drive()
+  {
+    State = MotorState.Drive;
+  }
+
+  public void Idle()
+  {
+    State = MotorState.Idle;
+  }
+
+  public abstract void Update(MotorContext context);
+}
