e14a5860279c4d1f18c439a7b82ac09a90395da0
Neomaster X <neomasterx@outlook.com>
2026-05-17 13:54:10 +0300

    #496 MotobugViewSystem.Update()

--- Assets/Scripts/Characters/Motobug/MotobugViewSystem.cs
+++ Assets/Scripts/Characters/Motobug/MotobugViewSystem.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using AnimatorParameters = SharedConsts.Animator.Parameters;
 
 public class MotobugViewSystem
 {
   private Animator _animator;
+  private MotobugViewContext _context;
 
   public void SetComponents(Animator animator)
   {
     _animator = animator;
+  }
+
+  public void Update(MotobugViewContext context)
+  {
+    _context = context;
+    UpdateAnimator();
+  }
+
+  private void UpdateAnimator()
+  {
+    _animator.SetBool(AnimatorParameters.Destroyed, _context.Destroyed);
   }
 }
