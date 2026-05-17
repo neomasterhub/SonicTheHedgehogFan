b18984fc0e5f4c266aadf671ab3d2c3f349ad39d
Neomaster X <neomasterx@outlook.com>
2026-05-17 23:09:19 +0300

    #499 MotorState

--- Assets/Scripts/Motors/IMotor.cs
+++ Assets/Scripts/Motors/IMotor.cs
@@ -1,4 +1,7 @@
 public interface IMotor : ISpeedSystem
 {
+  MotorState State { get; }
+  void Idle();
+  void Drive();
   void Update(MotorContext context);
 }
---
+++ Assets/Scripts/Motors/MotorState.cs
@@ -0,0 +1,5 @@
+public enum MotorState
+{
+  Idle,
+  Drive,
+}
