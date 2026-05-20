a1b9dbe5eecd358e73ed4ffc0253c2158d921862
Neomaster X <neomasterx@outlook.com>
2026-05-20 05:20:12 +0300

    #499 UDFEnemySensorSystem sensor

--- Assets/Scripts/Characters/Enemy/SensorSystems/UDFEnemySensorSystem.cs
+++ Assets/Scripts/Characters/Enemy/SensorSystems/UDFEnemySensorSystem.cs
@@ -2,10 +2,22 @@ using System;
 
 public class UDFEnemySensorSystem : IEnemySensorSystem
 {
+  private readonly UDFSensor _o;
+
   private Action _updateNext;
   private GroundDetectionResult? _ground;
   private WallDetectionResult? _leftWall;
   private WallDetectionResult? _rightWall;
+
+  public UDFEnemySensorSystem(Vector2 parentPosition)
+  {
+    _o = new(Color.red, parentPosition, Vector2.up, Vector2.down, Vector2.right);
+  }
+
+  public void Update(EnemySensorContext context)
+  {
+    _o.SetParentPosition(context.ParentPosition);
+  }
 
   public void Apply()
   {
