53c77e837dbb9f604e69a0f609729e7d38f7250b
Neomaster X <neomasterx@outlook.com>
2026-05-20 12:18:35 +0300

    #499 EnemySpeedContext.Speed

--- Assets/Scripts/Characters/Enemy/SpeedSystems/EnemySpeedContext.cs
+++ Assets/Scripts/Characters/Enemy/SpeedSystems/EnemySpeedContext.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public readonly struct EnemySpeedContext
 {
+  public readonly float Speed;
   public readonly float DirectionAngleDeg;
   public readonly float DirectionAngleRad;
 
-  public EnemySpeedContext(float directionAngleDeg)
+  public EnemySpeedContext(float speed, float directionAngleDeg)
   {
+    Speed = speed;
     DirectionAngleDeg = directionAngleDeg;
     DirectionAngleRad = directionAngleDeg * Mathf.Deg2Rad;
   }
