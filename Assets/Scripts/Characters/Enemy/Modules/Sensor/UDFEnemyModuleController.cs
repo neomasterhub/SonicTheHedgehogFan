d06cf4f3bc4b95b88879efc8100b73fa870397df
Neomaster X <neomasterx@outlook.com>
2026-05-21 23:48:05 +0300

    #499 UDFEnemyModuleController data

--- Assets/Scripts/Characters/Enemy/Modules/Sensor/UDFEnemyModuleController.cs
+++ Assets/Scripts/Characters/Enemy/Modules/Sensor/UDFEnemyModuleController.cs
@@ -1,6 +1,15 @@
+using UnityEngine;
+
 /// <summary>
 /// Data.
 /// </summary>
 public partial class UDFEnemyModuleController : EnemyModuleControllerBase
 {
+  private UDFSensor _o;
+  private WallDetectionResult? _wall;
+  private GroundDetectionResult? _ground;
+  private SpriteRenderer _spriteRenderer;
+
+  [SerializeField]
+  private Vector2 _position;
 }
