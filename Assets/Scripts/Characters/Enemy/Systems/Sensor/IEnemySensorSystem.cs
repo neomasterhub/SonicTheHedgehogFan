95cf9412855deeb4caa52f99b69246b22bb98ee4
Neomaster X <neomasterx@outlook.com>
2026-05-21 12:11:36 +0300

    #499 IEnemySensorSystem.SetParentPosition()

--- Assets/Scripts/Characters/Enemy/Systems/Sensor/IEnemySensorSystem.cs
+++ Assets/Scripts/Characters/Enemy/Systems/Sensor/IEnemySensorSystem.cs
@@ -1,6 +1,9 @@
+using UnityEngine;
+
 public interface IEnemySensorSystem<TContext>
   : IEnemySystem<EnemySensorSystemType, TContext>
 {
+  void SetParentPosition(Vector2 parentPosition);
   void Apply();
   void Draw();
 }
