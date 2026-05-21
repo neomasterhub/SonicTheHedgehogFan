298f0398993d6833b72b510717d4f6492af2911f
Neomaster X <neomasterx@outlook.com>
2026-05-21 20:06:42 +0300

    #499 EnemyController : IEnemy

--- Assets/Scripts/Characters/Enemy/EnemyController.cs
+++ Assets/Scripts/Characters/Enemy/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /// Data.
 /// </summary>
 [RequireComponent(typeof(BoxCollider2D))]
-public partial class EnemyController : MonoBehaviour
+public partial class EnemyController : MonoBehaviour, IEnemy
 {
   private readonly Pipeline _effects;
   private readonly TimerSystem _timerSystem;
@@ -20,4 +20,14 @@ public partial class EnemyController : MonoBehaviour
   [SerializeField]
   [InspectorLabel("Other Enemy")]
   private GameObject _otherEnemyObj;
+
+  public bool IsInvincible { get; private set; }
+  public bool IsAttacking { get; private set; }
+  public bool IsHit { get; set; }
+  public bool IsHurt { get; set; }
+  public float SpeedX { get; private set; }
+  public float SpeedY { get; private set; }
+  public float PositionX { get; private set; }
+  public float PositionY { get; private set; }
+  public IEnemy ContactEnemy { get; set; }
 }
