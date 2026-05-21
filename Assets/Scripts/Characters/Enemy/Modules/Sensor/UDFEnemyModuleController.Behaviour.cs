f6c3dc01d22f5a769df3181573a312ac7d042054
Neomaster X <neomasterx@outlook.com>
2026-05-21 23:49:33 +0300

    #499 UDFEnemyModuleController detect methods

--- Assets/Scripts/Characters/Enemy/Modules/Sensor/UDFEnemyModuleController.Behaviour.cs
+++ Assets/Scripts/Characters/Enemy/Modules/Sensor/UDFEnemyModuleController.Behaviour.cs
@@ -1,3 +1,6 @@
+using UnityEngine;
+using static SharedConsts.Physics;
+
 /// <summary>
 /// Behaviour.
 /// </summary>
@@ -5,5 +8,35 @@ public partial class UDFEnemyModuleController
 {
   public override void Apply()
   {
+    DetectWall(GroundLayer);
+    DetectGround(GroundLayer);
+  }
+
+  private void DetectWall(LayerMask groundLayer)
+  {
+    var hit = _o.FrontRay.Cast(groundLayer);
+
+    _wall = hit == null
+      ? null
+      : new(hit.Value.distance, Vector2.SignedAngle(-_o.FrontRay.Direction, hit.Value.normal).Round());
+  }
+
+  private void DetectGround(LayerMask groundLayer)
+  {
+    var hit = _o.DownRay.Cast(groundLayer);
+    if (hit != null)
+    {
+      _ground = new(false, hit.Value, Vector2.down, VerticalRelation.Above);
+      return;
+    }
+
+    hit = _o.UpRay.Cast(groundLayer);
+    if (hit != null)
+    {
+      _ground = new(false, hit.Value, Vector2.up, VerticalRelation.Below);
+      return;
+    }
+
+    _ground = null;
   }
 }
