37019da162427b5803d554e88c1ffd491d86fae6
Neomaster X <neomasterx@outlook.com>
2026-05-22 12:37:31 +0300

    #499 "Move" effect

--- Assets/Scripts/Characters/Enemy/Modules/AI/HorizontalPatrol/HorizontalPatrolAIEnemyModuleController.Effects.cs
+++ Assets/Scripts/Characters/Enemy/Modules/AI/HorizontalPatrol/HorizontalPatrolAIEnemyModuleController.Effects.cs
@@ -6,6 +6,7 @@ public partial class HorizontalPatrolAIEnemyModuleController
   protected override void SetEffectPipeline()
   {
     _effects.AddStep(CreateEffect_Stop());
+    _effects.AddStep(CreateEffect_Move());
   }
 
   private PipelineStep CreateEffect_Stop()
@@ -21,7 +22,26 @@ public partial class HorizontalPatrolAIEnemyModuleController
         _isStopped = true;
         _speed = -_speed;
         _timerSystem.StartIfNotRunning(_stopTimer);
+
         _context.IsStatic = true;
+        _context.Speed = 0;
+
+        return PipelineStepResult.Break;
+      })
+      .Build();
+  }
+
+  private PipelineStep CreateEffect_Move()
+  {
+    return PipelineStepBuilder.Create()
+      .WithDisplayName("Move")
+      .WithCondition(() =>
+        !_isStopped
+        && _context.IsStatic)
+      .WithAction(() =>
+      {
+        _context.IsStatic = false;
+        _context.Speed = _speed;
 
         return PipelineStepResult.Break;
       })
