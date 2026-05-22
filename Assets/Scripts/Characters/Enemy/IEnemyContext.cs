2a3da6aad291624187c175ec454c4a10691328aa
Neomaster X <neomasterx@outlook.com>
2026-05-22 11:58:56 +0300

    #499 IEnemyContext.IsStatic

--- Assets/Scripts/Characters/Enemy/IEnemyContext.cs
+++ Assets/Scripts/Characters/Enemy/IEnemyContext.cs
@@ -1,5 +1,6 @@
 public interface IEnemyContext : IEnemy
 {
+  bool IsStatic { get; set; }
   float Speed { get; set; }
   WallDetectionResult? Wall { get; set; }
   GroundDetectionResult? Ground { get; set; }
