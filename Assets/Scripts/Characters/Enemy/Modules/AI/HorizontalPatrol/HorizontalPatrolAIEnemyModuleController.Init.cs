cf942cb927ee716c98f52c68b15e2f7b9e8a12a3
Neomaster X <neomasterx@outlook.com>
2026-05-22 11:42:25 +0300

    #499 Horizontal patrol AI: init default patrol limits

--- Assets/Scripts/Characters/Enemy/Modules/AI/HorizontalPatrol/HorizontalPatrolAIEnemyModuleController.Init.cs
+++ Assets/Scripts/Characters/Enemy/Modules/AI/HorizontalPatrol/HorizontalPatrolAIEnemyModuleController.Init.cs
@@ -1,10 +1,20 @@
+using static EnemyConsts.Physics;
+using static SharedConsts.Physics;
+
 /// <summary>
-/// Data.
+/// Init.
 /// </summary>
 public partial class HorizontalPatrolAIEnemyModuleController
 {
-  [SerializeField]
-  private float _minPositionX;
-  [SerializeField]
-  private float _maxPositionX;
+  public override void Initialize(IEnemyContext context)
+  {
+    base.Initialize(context);
+
+    if (_minPositionX == 0 && _maxPositionX == 0)
+    {
+      var x = transform.position.x.Round(PositionRoundingDigits);
+      _minPositionX = x - DefaultPatrolRadius;
+      _maxPositionX = x + DefaultPatrolRadius;
+    }
+  }
 }
