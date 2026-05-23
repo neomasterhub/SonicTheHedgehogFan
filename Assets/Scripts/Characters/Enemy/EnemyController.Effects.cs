e5086bd534a8fb55ba7ab5dded7a556ae327b1ef
Neomaster X <neomasterx@outlook.com>
2026-05-23 18:53:06 +0300

    #499 remove enemy "Hit" effect

--- Assets/Scripts/Characters/Enemy/EnemyController.Effects.cs
+++ Assets/Scripts/Characters/Enemy/EnemyController.Effects.cs
@@ -6,7 +6,6 @@ public partial class EnemyController
   private void SetEffectPipeline()
   {
     _effects.AddStep(CreateEffect_Intersected());
-    _effects.AddStep(CreateEffect_Hit());
     _effects.AddStep(CreateEffect_GettingHit());
   }
 
@@ -20,26 +19,6 @@ public partial class EnemyController
           || !_collider.bounds.Intersects(_otherEnemyCollider.bounds)
           ? PipelineStepResult.Break
           : PipelineStepResult.Continue;
-      })
-      .Build();
-  }
-
-  private PipelineStep CreateEffect_Hit()
-  {
-    return PipelineStepBuilder.Create()
-      .WithDisplayName("Hit")
-      .WithCondition(() =>
-        _isAlive
-        && !_otherEnemy.IsInvincible
-        && !_otherEnemy.IsAttacking
-        && !_otherEnemy.IsHurt)
-      .WithAction(() =>
-      {
-        _otherEnemy.IsHit = true;
-        _otherEnemy.IsHurt = true;
-        _otherEnemy.ContactEnemy = this;
-
-        return PipelineStepResult.Break;
       })
       .Build();
   }
