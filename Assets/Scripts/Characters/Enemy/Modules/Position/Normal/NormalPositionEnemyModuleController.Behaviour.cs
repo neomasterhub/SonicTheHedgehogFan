2cd1dcd787dfebbe9e9be0f5ff0ca512c99c88a6
Neomaster X <neomasterx@outlook.com>
2026-05-24 00:22:02 +0300

    #499 normal position module: SetPosition()

--- Assets/Scripts/Characters/Enemy/Modules/Position/Normal/NormalPositionEnemyModuleController.Behaviour.cs
+++ Assets/Scripts/Characters/Enemy/Modules/Position/Normal/NormalPositionEnemyModuleController.Behaviour.cs
@@ -1,3 +1,6 @@
+using UnityEngine;
+using static SharedConsts.Physics;
+
 /// <summary>
 /// Behaviour.
 /// </summary>
@@ -5,5 +8,22 @@ public partial class NormalPositionEnemyModuleController
 {
   public override void Apply()
   {
+    SetPosition();
+  }
+
+  private void SetPosition()
+  {
+    var speedY = _context.SpeedY;
+
+    if (_context.Ground.HasValue)
+    {
+      var ground = _context.Ground.Value;
+
+      speedY -= (ground.Distance
+        * (int)ground.SensorGroundRelation)
+        - GroundedPositionUpwardOffset;
+    }
+
+    transform.position += new Vector3(_context.SpeedX, speedY);
   }
 }
