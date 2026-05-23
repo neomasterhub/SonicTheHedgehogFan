9cf6077740a85db0d5653a190a1ac8b2eab878b5
Neomaster X <neomasterx@outlook.com>
2026-05-23 09:48:08 +0300

    #499 Copy DetectGround() to shared helpers

--- Assets/Scripts/Shared/Helpers.cs
+++ Assets/Scripts/Shared/Helpers.cs
@@ -9,4 +9,90 @@ public static class Helpers
       return new Vector3(x, y, z);
     }
   }
+
+  public static class Sensors
+  {
+    public GroundDetectionResult? DetectGround(bool horizontalDirection, LayerMask groundLayer)
+    {
+      SensorRay dr1;
+      SensorRay dr2;
+
+      if (horizontalDirection)
+      {
+        dr1 = _a.DownRay;
+        dr2 = _b.DownRay;
+      }
+      else
+      {
+        dr1 = _b.DownRay;
+        dr2 = _a.DownRay;
+      }
+
+      var dr1Hit = dr1.Cast(groundLayer);
+      var dr2Hit = dr2.Cast(groundLayer);
+
+      if (dr1Hit != null && dr2Hit != null)
+      {
+        if (dr1Hit.Value.distance <= dr2Hit.Value.distance)
+        {
+          return new(!horizontalDirection, dr1Hit.Value, dr1.Direction);
+        }
+        else
+        {
+          return new(horizontalDirection, dr2Hit.Value, dr2.Direction);
+        }
+      }
+
+      SensorRay ur1;
+      SensorRay ur2;
+
+      if (horizontalDirection)
+      {
+        ur1 = _a.UpRay;
+        ur2 = _b.UpRay;
+      }
+      else
+      {
+        ur1 = _b.UpRay;
+        ur2 = _a.UpRay;
+      }
+
+      var ur1Hit = ur1.Cast(groundLayer);
+      var ur2Hit = ur2.Cast(groundLayer);
+
+      if (ur1Hit != null && ur2Hit != null)
+      {
+        if (ur1Hit.Value.distance >= ur2Hit.Value.distance)
+        {
+          return new(!horizontalDirection, ur1Hit.Value, ur1.Direction, VerticalRelation.Below);
+        }
+        else
+        {
+          return new(horizontalDirection, ur2Hit.Value, ur2.Direction, VerticalRelation.Below);
+        }
+      }
+
+      if (ur1Hit != null)
+      {
+        return new(!horizontalDirection, ur1Hit.Value, ur1.Direction, VerticalRelation.Below, IsBalancing(groundLayer));
+      }
+
+      if (ur2Hit != null)
+      {
+        return new(horizontalDirection, ur2Hit.Value, ur2.Direction, VerticalRelation.Below, IsBalancing(groundLayer));
+      }
+
+      if (dr1Hit != null)
+      {
+        return new(!horizontalDirection, dr1Hit.Value, dr1.Direction, VerticalRelation.Above, IsBalancing(groundLayer));
+      }
+
+      if (dr2Hit != null)
+      {
+        return new(horizontalDirection, dr2Hit.Value, dr2.Direction, VerticalRelation.Above, IsBalancing(groundLayer));
+      }
+
+      return null;
+    }
+  }
 }
