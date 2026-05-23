6cfd922f7421004931f69f738721c10dcefbd432
Neomaster X <neomasterx@outlook.com>
2026-05-24 02:03:03 +0300

    #499 fix: improve enemy DefaultSpeedSpx

--- Assets/Scripts/Characters/Enemy/EnemyConsts.cs
+++ Assets/Scripts/Characters/Enemy/EnemyConsts.cs
@@ -5,7 +5,7 @@ public static class EnemyConsts
   public static class Physics
   {
     public const float DeadActiveTimer = 1;
-    public const float DefaultSpeedSpx = 16;
+    public const float DefaultSpeedSpx = 128;
     public const float DefaultPatrolRadius = 10;
     public const float DefaultPatrolStopTimer = 3;
     public static readonly Vector3 UDFLengths = new(0.2f, 0.1f, 0.5f);
