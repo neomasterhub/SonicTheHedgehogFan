5ab295acf50831bc7cb49a4adf7a318952019561
Neomaster X <neomasterx@outlook.com>
2026-05-24 09:09:49 +0300

    #506 enemy speed module with gravity and max fall speeds

--- Assets/Scripts/Characters/Enemy/Modules/Speed/Ground/GroundSpeedEnemyModuleController.cs
+++ Assets/Scripts/Characters/Enemy/Modules/Speed/Ground/GroundSpeedEnemyModuleController.cs
@@ -1,7 +1,16 @@
+using UnityEngine;
+
 /// <summary>
 /// Data.
 /// </summary>
 public partial class GroundSpeedEnemyModuleController
   : EnemyModuleControllerBase
 {
+  private float _gravitySpeed;
+  private float _maxFallSpeed;
+
+  [SerializeField]
+  private float _gravitySpeedSpx;
+  [SerializeField]
+  private float _maxFallSpeedPx;
 }
