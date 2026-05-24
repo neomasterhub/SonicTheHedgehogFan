58432fc372d5822b39ee79c24f5450e0956d459d
Neomaster X <neomasterx@outlook.com>
2026-05-24 11:45:35 +0300

    #509 enemy "Death" effect

--- Assets/Scripts/Characters/Enemy/Modules/AI/Defeat/DefeatAIEnemyModuleController.Effects.cs
+++ Assets/Scripts/Characters/Enemy/Modules/AI/Defeat/DefeatAIEnemyModuleController.Effects.cs
@@ -5,5 +5,24 @@ public partial class DefeatAIEnemyModuleController
 {
   protected override void SetEffectPipeline()
   {
+    _effects.AddStep(CreateEffect_Death());
+  }
+
+  private PipelineStep CreateEffect_Death()
+  {
+    return PipelineStepBuilder.Create()
+      .WithDisplayName("Death")
+      .WithCondition(() =>
+        _context.Health < 0
+        && !_context.IsDead)
+      .WithAction(() =>
+      {
+        _context.IsDead = true;
+        _context.IsStatic = true;
+        _context.Speed = 0;
+
+        return PipelineStepResult.Break;
+      })
+      .Build();
   }
 }
