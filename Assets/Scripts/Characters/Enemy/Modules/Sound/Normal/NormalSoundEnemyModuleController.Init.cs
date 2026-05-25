576dbc6504c5c0a0e918c6ca0f57d63aed8cc8b8
Neomaster X <neomasterx@outlook.com>
2026-05-25 12:16:17 +0300

    #521 sound module: attacked and dying audio clips

--- Assets/Scripts/Characters/Enemy/Modules/Sound/Normal/NormalSoundEnemyModuleController.Init.cs
+++ Assets/Scripts/Characters/Enemy/Modules/Sound/Normal/NormalSoundEnemyModuleController.Init.cs
@@ -1,3 +1,6 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
 /// <summary>
 /// Init.
 /// </summary>
@@ -5,5 +8,22 @@ public partial class NormalSoundEnemyModuleController
 {
   protected override void SetSounds()
   {
+    if (_attackedAudioClip != null)
+    {
+      var audioSource = this.AddComponent<AudioSource>();
+      audioSource.clip = _attackedAudioClip;
+      _sounds.Add(new(
+        audioSource,
+        () => _context.IsHit && !_context.IsDying));
+    }
+
+    if (_dyingAudioClip != null)
+    {
+      var audioSource = this.AddComponent<AudioSource>();
+      audioSource.clip = _dyingAudioClip;
+      _sounds.Add(new(
+        audioSource,
+        () => _context.IsHit && _context.IsDying));
+    }
   }
 }
