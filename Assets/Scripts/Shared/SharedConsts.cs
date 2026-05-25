0d3b0fc477be49d6125907d58c1c00fff713b1c3
Neomaster X <neomasterx@outlook.com>
2026-05-25 10:54:40 +0300

    #518 ref: sort SharedConsts classes by name

--- Assets/Scripts/Shared/SharedConsts.cs
+++ Assets/Scripts/Shared/SharedConsts.cs
@@ -3,45 +3,6 @@ using static PlayerInput;
 
 public static class SharedConsts
 {
-  public static class ConvertValues
-  {
-    public const int FramePerSec = 60;
-    public const int PxPerUnit = 40;
-    public const int SpxPerPx = 256;
-    public const int SpxPerUnit = SpxPerPx * PxPerUnit;
-  }
-
-  public static class Colors
-  {
-    public static readonly Color TransparentBlack = new(0, 0, 0, 0);
-  }
-
-  public static class Rendering
-  {
-    public const float DebugScale = 10000;
-    public const int PlayerOrderInLayer = 1;
-  }
-
-  public static class Input
-  {
-    public const int PressedHistoryCapacity = 10;
-  }
-
-  public static class Physics
-  {
-    public const int SpeedRoundingDigits = 3;
-    public const int PositionRoundingDigits = 3;
-    public const float PositionBackwardOffset = 0.02f;
-    public const float GroundedPositionUpwardOffset = 0.05f;
-    public static readonly LayerMask GroundLayer = 1 << 3;
-
-    public static class GroundAngleRanges
-    {
-      public static readonly Range Flat = new(-23, 23);
-      public static readonly Range Slope = new(-45, 45);
-    }
-  }
-
   public static class Animator
   {
     public static class Parameters
@@ -66,6 +27,45 @@ public static class SharedConsts
       public const string Rolling = nameof(Rolling);
       public const string Walking = nameof(Walking);
     }
+  }
+
+  public static class Colors
+  {
+    public static readonly Color TransparentBlack = new(0, 0, 0, 0);
+  }
+
+  public static class ConvertValues
+  {
+    public const int FramePerSec = 60;
+    public const int PxPerUnit = 40;
+    public const int SpxPerPx = 256;
+    public const int SpxPerUnit = SpxPerPx * PxPerUnit;
+  }
+
+  public static class Input
+  {
+    public const int PressedHistoryCapacity = 10;
+  }
+
+  public static class Physics
+  {
+    public const int SpeedRoundingDigits = 3;
+    public const int PositionRoundingDigits = 3;
+    public const float PositionBackwardOffset = 0.02f;
+    public const float GroundedPositionUpwardOffset = 0.05f;
+    public static readonly LayerMask GroundLayer = 1 << 3;
+
+    public static class GroundAngleRanges
+    {
+      public static readonly Range Flat = new(-23, 23);
+      public static readonly Range Slope = new(-45, 45);
+    }
+  }
+
+  public static class Rendering
+  {
+    public const float DebugScale = 10000;
+    public const int PlayerOrderInLayer = 1;
   }
 
   public static class SecretCodes
