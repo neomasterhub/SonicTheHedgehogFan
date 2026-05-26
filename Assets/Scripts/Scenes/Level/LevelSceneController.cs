b749e288bf662a34e3536cf6ddeeec89cce072f3
Neomaster X <neomasterx@outlook.com>
2026-05-26 14:13:00 +0300

    #529 LevelSceneController debug mode flag

--- Assets/Scripts/Scenes/Level/LevelSceneController.cs
+++ Assets/Scripts/Scenes/Level/LevelSceneController.cs
@@ -1,6 +1,10 @@
+using UnityEngine;
+
 /// <summary>
 /// Data.
 /// </summary>
 public partial class LevelSceneController : SceneControllerBase
 {
+  [SerializeField]
+  private bool _debugMode;
 }
