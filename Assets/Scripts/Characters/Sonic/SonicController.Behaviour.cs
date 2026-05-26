0a8f3a072f746dd27e790de3fce899cfb9d29201
Neomaster X <neomasterx@outlook.com>
2026-05-26 10:21:38 +0300

    #503 SonicController.Behaviour

---
+++ Assets/Scripts/Characters/Sonic/SonicController.Behaviour.cs
@@ -0,0 +1,44 @@
+/// <summary>
+/// Behaviour.
+/// </summary>
+public partial class SonicController
+{
+  // Camera target
+  public bool IsDead => _isDead;
+  public bool IsDying => _isDying;
+
+  // Enemy
+  public bool IsInvincible { get; private set; }
+  public bool IsAttacking { get; private set; }
+  public bool IsHit { get; set; }
+  public bool IsHurt { get; set; }
+  public int Health { get; }
+  public float SpeedX => _speedSystem.SpeedX;
+  public float SpeedY => _speedSystem.SpeedY;
+  public float PositionX => transform.position.x;
+  public float PositionY => transform.position.y;
+  public IEnemy ContactEnemy { get; set; }
+
+  // Look vertical direction provider
+  public VerticalDirection LookVerticalDirection
+  {
+    get
+    {
+      if (_isCurlingUp)
+      {
+        return VerticalDirection.Down;
+      }
+
+      if (_isLookingUp)
+      {
+        return VerticalDirection.Up;
+      }
+
+      return VerticalDirection.None;
+    }
+  }
+
+  // Ring collector
+  public bool CanCollectRing { get; private set; }
+  public ICollector Rings { get; }
+}
