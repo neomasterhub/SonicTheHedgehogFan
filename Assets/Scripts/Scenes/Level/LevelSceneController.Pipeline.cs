63d9df35b07ec88f3155591c29918d8def6aa724
Neomaster X <neomasterx@outlook.com>
2026-05-27 00:48:25 +0300

    #529 Set debug mode from scene

--- Assets/Scripts/Scenes/Level/LevelSceneController.Pipeline.cs
+++ Assets/Scripts/Scenes/Level/LevelSceneController.Pipeline.cs
@@ -22,6 +22,10 @@ public partial class LevelSceneController
     if (_inputSystem.CheckLastPressed(ToggleDebugMode))
     {
       _debugMode = !_debugMode;
+      for (var i = 0; i < _debugObjects.Length; i++)
+      {
+        _debugObjects[i].DebugMode = _debugMode;
+      }
     }
   }
 }
