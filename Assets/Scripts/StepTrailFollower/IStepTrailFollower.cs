5340ecb42eb2225d88c19c4bf2593f3a7e5ee77e
Neomaster X <neomasterx@outlook.com>
2026-05-27 10:11:15 +0300

    #546 StepTrailFollowerController  : IStepTrailFollower

---
+++ Assets/Scripts/StepTrailFollower/IStepTrailFollower.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IStepTrailFollower
+{
+  Vector3 Origin { set; }
+}
--- Assets/Scripts/StepTrailFollower/StepTrailFollowerController.Behaviour.cs
+++ Assets/Scripts/StepTrailFollower/StepTrailFollowerController.Behaviour.cs
@@ -1,6 +1,10 @@
+using UnityEngine;
+
 /// <summary>
 /// Behaviour.
 /// </summary>
 public partial class StepTrailFollowerController
+  : IStepTrailFollower
 {
+  public Vector3 Origin { set => _origin = value; }
 }
