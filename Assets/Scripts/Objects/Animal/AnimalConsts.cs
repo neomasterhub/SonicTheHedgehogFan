9c46700c4b60a92b757600511f89bd4e21ed2064
Neomaster X <neomasterx@outlook.com>
2026-05-29 16:59:08 +0300

    #549 Improve AnimalConsts

--- Assets/Scripts/Objects/Animal/AnimalConsts.cs
+++ Assets/Scripts/Objects/Animal/AnimalConsts.cs
@@ -1,8 +1,8 @@
 public static class AnimalConsts
 {
   public const float SensorY = -0.3f;
-  public const float SpeedXPx = 2;
-  public const float JumpSpeedPx = 4;
-  public const float ReleaseSpeedPx = 5;
-  public const float GravitySpeedSpx = 58;
+  public const float SpeedXPx = 3;
+  public const float JumpSpeedPx = 6;
+  public const float ReleaseSpeedPx = 4;
+  public const float GravitySpeedSpx = 56;
 }
