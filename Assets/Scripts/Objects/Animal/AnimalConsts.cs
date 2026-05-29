59d85297debfd7748850e0e863f7127b6115eb0b
Neomaster X <neomasterx@outlook.com>
2026-05-29 11:58:45 +0300

    #549 AnimalConsts

---
+++ Assets/Scripts/Objects/Animal/AnimalConsts.cs
@@ -0,0 +1,7 @@
+using static SharedConsts.ConvertValues;
+
+public static class AnimalConsts
+{
+  public const float JumpSpeed = 4 / PxPerUnit;
+  public const float GravitySpeed = 24f / SpxPerUnit;
+}
