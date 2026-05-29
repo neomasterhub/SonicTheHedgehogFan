6e7db17e50a00eb6ac84c0994376da9ad6345e82
Neomaster X <neomasterx@outlook.com>
2026-05-29 11:47:41 +0300

    #549 AnimalSpeedSystem base

--- Assets/Scripts/Objects/Animal/AnimalSpeedSystem.cs
+++ Assets/Scripts/Objects/Animal/AnimalSpeedSystem.cs
@@ -1,3 +1,28 @@
 public class AnimalSpeedSystem : SpeedSystemBase
 {
+  private AnimalSpeedContext _context;
+
+  public void SetSpeed(AnimalSpeedContext context)
+  {
+    _context = context;
+
+    if (context.IsGrounded)
+    {
+      SetSpeed_Grounded();
+    }
+    else
+    {
+      SetSpeed_Airborne();
+    }
+
+    RoundSpeed();
+  }
+
+  private void SetSpeed_Airborne()
+  {
+  }
+
+  private void SetSpeed_Grounded()
+  {
+  }
 }
