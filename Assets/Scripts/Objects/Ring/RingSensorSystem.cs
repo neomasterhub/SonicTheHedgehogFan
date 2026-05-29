0bb7991ff8cca76dd578e6a5cb3ee99d806863ea
Neomaster X <neomasterx@outlook.com>
2026-05-29 10:46:07 +0300

    #549 ref: RingSensorSystem: naming

--- Assets/Scripts/Objects/Ring/RingSensorSystem.cs
+++ Assets/Scripts/Objects/Ring/RingSensorSystem.cs
@@ -4,29 +4,29 @@ using static RingConsts.Physics;
 
 public class RingSensorSystem
 {
-  private readonly UDSensor _o;
-  private readonly Vector2 _oOffset;
+  private readonly UDSensor _a;
+  private readonly Vector2 _aOffset;
 
   public RingSensorSystem()
   {
-    _oOffset = new(0, -SensorY);
-    _o = new(Color.gold, _oOffset, Vector2.up, Vector2.down);
-    _o.UpRay.Length = InnerSensorRayLength;
-    _o.DownRay.Length = OuterSensorRayLength;
+    _aOffset = new(0, -SensorY);
+    _a = new(Color.gold, _aOffset, Vector2.up, Vector2.down);
+    _a.UpRay.Length = InnerSensorRayLength;
+    _a.DownRay.Length = OuterSensorRayLength;
   }
 
   public void Draw()
   {
-    _o.Draw();
+    _a.Draw();
   }
 
   public void Update(Vector2 parentPosition)
   {
-    _o.SetParentPosition(parentPosition + _oOffset);
+    _a.SetParentPosition(parentPosition + _aOffset);
   }
 
   public GroundDetectionResult? DetectGround(LayerMask groundLayer, char sensorId = 'A')
   {
-    return ADetectGround(sensorId, _o, groundLayer);
+    return ADetectGround(sensorId, _a, groundLayer);
   }
 }
