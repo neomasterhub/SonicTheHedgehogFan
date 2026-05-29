7692d0212c7182079e043471bd8fc59810a3c805
Neomaster X <neomasterx@outlook.com>
2026-05-29 13:14:47 +0300

    #549 AnimalConsts in px and spx

--- Assets/Scripts/Objects/Animal/AnimalConsts.cs
+++ Assets/Scripts/Objects/Animal/AnimalConsts.cs
@@ -1,9 +1,8 @@
-using static SharedConsts.ConvertValues;
-
 public static class AnimalConsts
 {
   public const float SensorY = -0.3f;
-  public const float SpeedX = 1 / PxPerUnit;
-  public const float JumpSpeed = 4 / PxPerUnit;
-  public const float GravitySpeed = 24 / SpxPerUnit;
+  public const float SpeedXPx = 2;
+  public const float JumpSpeedPx = 4;
+  public const float ReleasedSpeedPx = 5;
+  public const float GravitySpeedSpx = 58;
 }
