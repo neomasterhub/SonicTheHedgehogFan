50ce360b9c838366842edd3d02357774040b7124
Neomaster X <neomasterx@outlook.com>
2026-06-01 08:22:53 +0300

    #573 ref: InitializeComponents(): use random Next with single arg

--- Assets/Scripts/Characters/Enemy/Modules/AnimalContainer/AnimalContainerEnemyModuleController.Init.cs
+++ Assets/Scripts/Characters/Enemy/Modules/AnimalContainer/AnimalContainerEnemyModuleController.Init.cs
@@ -22,7 +22,7 @@ public partial class AnimalContainerEnemyModuleController
   {
     if (_animalObj == null)
     {
-      var animalPrefab = _animalPrefabs[SystemRandom.Next(0, _animalPrefabs.Length)];
+      var animalPrefab = _animalPrefabs[SystemRandom.Next(_animalPrefabs.Length)];
       animalPrefab.SetActive(false);
 
       _animalObj = Instantiate(animalPrefab, transform.position + _animalBreakOutOrigin, Quaternion.identity);
