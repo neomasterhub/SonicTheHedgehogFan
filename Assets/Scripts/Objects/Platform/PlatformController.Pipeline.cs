5048de3481e1ee13949bd09192e169bd26138417
Neomaster X <neomasterx@outlook.com>
2026-06-04 07:44:43 +0300

    #443 PlatformController: pipeline class base

---
+++ Assets/Scripts/Objects/Platform/PlatformController.Pipeline.cs
@@ -0,0 +1,19 @@
+/// <summary>
+/// Pipeline.
+/// </summary>
+public partial class PlatformController
+{
+  private void FixedUpdate()
+  {
+    ApplyMovement();
+    UpdatePosition();
+  }
+
+  private void ApplyMovement()
+  {
+  }
+
+  private void UpdatePosition()
+  {
+  }
+}
