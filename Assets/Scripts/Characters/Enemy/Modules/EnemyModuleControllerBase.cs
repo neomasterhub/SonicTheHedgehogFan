26a05b04543cb5cb2d888b9e3a5848504ae90b91
Neomaster X <neomasterx@outlook.com>
2026-06-09 12:26:53 +0300

    #443 ref: ModuleControllerBase<TCore>

--- Assets/Scripts/Characters/Enemy/Modules/EnemyModuleControllerBase.cs
+++ Assets/Scripts/Characters/Enemy/Modules/EnemyModuleControllerBase.cs
@@ -2,20 +2,6 @@ using UnityEngine;
 
 [RequireComponent(typeof(EnemyControllerBase))]
 public abstract class EnemyModuleControllerBase
-  : MonoBehaviour,
-  ISystemModule<EnemyControllerBase>
+  : ModuleControllerBase<EnemyControllerBase>
 {
-  protected EnemyControllerBase _context;
-
-  private void Awake()
-  {
-    Initialize(GetComponent<EnemyControllerBase>());
-  }
-
-  public virtual void Initialize(EnemyControllerBase context)
-  {
-    _context = context;
-  }
-
-  public abstract void Apply();
 }
