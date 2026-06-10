16f826161da2efe237ccf727c467cddab04bfee6
Neomaster X <neomasterx@outlook.com>
2026-06-10 08:35:28 +0300

    #588 extensions: Vector3.RoundPosition2D()

--- Assets/Scripts/Extensions/MathExtensions.cs
+++ Assets/Scripts/Extensions/MathExtensions.cs
@@ -1,9 +1,16 @@
 using System;
+using UnityEngine;
+using static SharedConsts.Physics;
 
 public static class MathExtensions
 {
   public static float Round(this float x, int digits = 0)
   {
     return MathF.Round(x, digits);
+  }
+
+  public static Vector3 RoundPosition2D(this Vector3 v)
+  {
+    return new(v.x.Round(PositionRoundingDigits), v.y.Round(PositionRoundingDigits));
   }
 }
