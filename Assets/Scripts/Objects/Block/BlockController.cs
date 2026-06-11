1cf07c3105dfe737231c3f9967b5ff4b4ded60f9
Neomaster X <neomasterx@outlook.com>
2026-06-11 11:21:35 +0300

    #592 Block data fields

--- Assets/Scripts/Objects/Block/BlockController.cs
+++ Assets/Scripts/Objects/Block/BlockController.cs
@@ -1,6 +1,12 @@
+using UnityEngine;
+
 /// <summary>
 /// Data.
 /// </summary>
+[RequireComponent(typeof(Collider2D))]
 public partial class BlockController : BlockControllerBase
 {
+  private Collider2D _collider;
+  private Collider2D _playerCollider;
+  private IBlockPlayer _player;
 }
