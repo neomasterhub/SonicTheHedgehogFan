f8bdd8dc8a8a74ba66449c12912c13ab9efd058d
Neomaster X <neomasterx@outlook.com>
2026-06-12 17:17:29 +0300

    #592 IBlockPlayer.SpeedX

--- Assets/Scripts/Objects/Block/IBlockPlayer.cs
+++ Assets/Scripts/Objects/Block/IBlockPlayer.cs
@@ -5,4 +5,5 @@ public interface IBlockPlayer
   Transform ContactLeftWallTransform { get; }
   Transform ContactRightWallTransform { get; }
   IBlock ContactBlock { set; }
+  float SpeedX { get; }
 }
