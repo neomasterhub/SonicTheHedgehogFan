e47307624313912a2840879cb90e3d8a1b7c58bb
Neomaster X <neomasterx@outlook.com>
2026-06-12 18:04:38 +0300

    #592 Push speed in px in inspector

--- Assets/Scripts/Objects/Block/BlockController.Init.cs
+++ Assets/Scripts/Objects/Block/BlockController.Init.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using static SharedConsts;
+using static SharedConsts.ConvertValues;
 
 /// <summary>
 /// Init.
@@ -9,11 +10,17 @@ public partial class BlockController
   private void Awake()
   {
     InitializeComponents();
+    InitializeData();
   }
 
   private void InitializeComponents()
   {
     var player = GameObject.FindWithTag(Tags.Player);
     _player = player.GetComponent<IBlockPlayer>();
+  }
+
+  private void InitializeData()
+  {
+    PushSpeed = _pushSpeedPx / PxPerUnit;
   }
 }
--- Assets/Scripts/Objects/Block/BlockController.cs
+++ Assets/Scripts/Objects/Block/BlockController.cs
@@ -1,7 +1,12 @@
+using UnityEngine;
+
 /// <summary>
 /// Data.
 /// </summary>
 public partial class BlockController : BlockControllerBase
 {
   private IBlockPlayer _player;
+
+  [SerializeField]
+  private float _pushSpeedPx;
 }
