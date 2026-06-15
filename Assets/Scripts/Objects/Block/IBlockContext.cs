f7a842d3a86d029ce2561b278c85cc9e6ce98313
Neomaster X <neomasterx@outlook.com>
2026-06-15 08:02:05 +0300

    #592 IBlockContext with detection results

--- Assets/Scripts/Objects/Block/IBlockContext.cs
+++ Assets/Scripts/Objects/Block/IBlockContext.cs
@@ -1,3 +1,6 @@
 public interface IBlockContext : IBlock
 {
+  GroundDetectionResult? Ground { get; set; }
+  WallDetectionResult? LeftWall { get; set; }
+  WallDetectionResult? RightWall { get; set; }
 }
