ce1f66fa1791037e50894ea57d472e761a76fefe
Neomaster X <neomasterx@outlook.com>
2026-06-15 19:53:23 +0300

    #592 AB sensor module: UpdatePushSpeed() draft

--- Assets/Scripts/Objects/Block/Modules/Sensor/ABSensorBlockModuleController.Behaviour.cs
+++ Assets/Scripts/Objects/Block/Modules/Sensor/ABSensorBlockModuleController.Behaviour.cs
@@ -10,6 +10,7 @@ public partial class ABSensorBlockModuleController
   public override void Apply()
   {
     ApplySensors();
+    UpdatePushSpeed();
   }
 
   private void ApplySensors()
@@ -20,6 +21,22 @@ public partial class ABSensorBlockModuleController
     _context.LeftWall = DetectWall(_a, GroundLayer);
     _context.RightWall = DetectWall(_b, GroundLayer);
     _context.Ground = ABDetectGround(_a, _b, GroundLayer, !_spriteRenderer.flipX);
+  }
+
+  private void UpdatePushSpeed()
+  {
+    if (_context.LeftWall == null)
+    {
+      _context.PushSpeed = _maxPushSpeed;
+    }
+    else
+    {
+      var dist = _context.LeftWall.Value.Distance.Round(SpeedRoundingDigits);
+      if (dist < _maxPushSpeed)
+      {
+        _context.PushSpeed = dist;
+      }
+    }
   }
 
   private WallDetectionResult? DetectWall(UDFSensor sensor, LayerMask groundLayer)
