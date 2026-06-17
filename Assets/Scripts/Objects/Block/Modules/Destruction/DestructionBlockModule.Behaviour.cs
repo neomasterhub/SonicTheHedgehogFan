fd627e6a54edd1451c0c33e3d74045fd195c583d
Neomaster X <neomasterx@outlook.com>
2026-06-17 18:06:09 +0300

    #592 remove ToggleBlockLayer()

--- Assets/Scripts/Objects/Block/Modules/Destruction/DestructionBlockModule.Behaviour.cs
+++ Assets/Scripts/Objects/Block/Modules/Destruction/DestructionBlockModule.Behaviour.cs
@@ -1,6 +1,3 @@
-using static BlockConsts;
-using static SharedConsts.Physics;
-
 /// <summary>
 /// Behaviour.
 /// </summary>
@@ -8,28 +5,5 @@ public partial class DestructionBlockModule
 {
   public override void Apply()
   {
-    ToggleBlockLayer();
-  }
-
-  private void ToggleBlockLayer()
-  {
-    if (_player.ContactBlock != null
-      || !_player.IsRolling)
-    {
-      gameObject.layer = BlockLayerIndex;
-      return;
-    }
-
-    var pSpeedX = _player.SpeedX;
-
-    if (_player.IsGrounded
-      && (pSpeedX > MinDestructionPlayerSpeedX
-      || pSpeedX < -MinDestructionPlayerSpeedX))
-    {
-      gameObject.layer = 0;
-      return;
-    }
-
-    gameObject.layer = BlockLayerIndex;
   }
 }
