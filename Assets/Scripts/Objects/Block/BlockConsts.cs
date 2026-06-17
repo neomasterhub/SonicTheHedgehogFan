3eacd706f57d311b91abfaf6892452a9a67b025f
Neomaster X <neomasterx@outlook.com>
2026-06-17 17:32:04 +0300

    #592 MinDestructionPlayerSpeedX

--- Assets/Scripts/Objects/Block/BlockConsts.cs
+++ Assets/Scripts/Objects/Block/BlockConsts.cs
@@ -1,5 +1,8 @@
+using static SharedConsts.ConvertValues;
+
 public static class BlockConsts
 {
   public const float GravitySpeedSpx = 56;
   public const float MaxFallSpeedPx = 16;
+  public const float MinDestructionPlayerSpeedX = 1 / PxPerUnit;
 }
