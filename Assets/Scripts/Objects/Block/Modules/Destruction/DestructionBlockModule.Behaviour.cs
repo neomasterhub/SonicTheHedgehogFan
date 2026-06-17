29369e6754c6376ceb190e9f6456c1528a78fd8d
Neomaster X <neomasterx@outlook.com>
2026-06-17 15:00:48 +0300

    #592 ToggleBlockLayer()

--- Assets/Scripts/Objects/Block/Modules/Destruction/DestructionBlockModule.Behaviour.cs
+++ Assets/Scripts/Objects/Block/Modules/Destruction/DestructionBlockModule.Behaviour.cs
@@ -5,9 +5,25 @@ public partial class DestructionBlockModule
 {
   public override void Apply()
   {
+    ToggleBlockLayer();
   }
 
   private void ToggleBlockLayer()
   {
+    if (_player.ContactBlock != null)
+    {
+      return;
+    }
+
+    if (_player.IsRolling
+      && (_player.SpeedX > 0.02f
+      || _player.SpeedX < -0.02f))
+    {
+      gameObject.layer = 0;
+    }
+    else
+    {
+      gameObject.layer = 6;
+    }
   }
 }
