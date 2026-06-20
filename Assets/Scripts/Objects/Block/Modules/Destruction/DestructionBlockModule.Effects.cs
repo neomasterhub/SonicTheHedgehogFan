a62734f71fd720ba28bab9147bbf51d5d5bfea07
Neomaster X <neomasterx@outlook.com>
2026-06-20 04:09:17 +0300

    #592 Destruction module: "Intersect" effect

--- Assets/Scripts/Objects/Block/Modules/Destruction/DestructionBlockModule.Effects.cs
+++ Assets/Scripts/Objects/Block/Modules/Destruction/DestructionBlockModule.Effects.cs
@@ -5,5 +5,20 @@ public partial class DestructionBlockModule
 {
   private void SetEffectPipeline()
   {
+    _effects.AddStep(CreateEffect_Intersect());
+  }
+
+  private PipelineStep CreateEffect_Intersect()
+  {
+    return PipelineStepBuilder.Create()
+      .WithDisplayName("Intersect")
+      .WithAction(() =>
+      {
+        return _layer == 0
+          && _collider.bounds.Intersects(_playerCollider.bounds)
+          ? PipelineStepResult.Continue
+          : PipelineStepResult.Break;
+      })
+      .Build();
   }
 }
