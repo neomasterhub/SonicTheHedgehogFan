8ebec69e394d56be8a6a37bc7f5c2553920b2841
Neomaster X <neomasterx@outlook.com>
2026-06-21 19:35:34 +0300

    #592 OneShot destruction module: "Attacked" effect

--- Assets/Scripts/Objects/Block/Modules/Destruction/OneShot/OneShotDestructionBlockModule.Effects.cs
+++ Assets/Scripts/Objects/Block/Modules/Destruction/OneShot/OneShotDestructionBlockModule.Effects.cs
@@ -5,5 +5,25 @@ public partial class OneShotDestructionBlockModule
 {
   private void SetEffectPipeline()
   {
+    _effects.AddStep(CreateEffect_Attacked());
+  }
+
+  private PipelineStep CreateEffect_Attacked()
+  {
+    return PipelineStepBuilder.Create()
+      .WithDisplayName("Attacked")
+      .WithCondition(() =>
+        _playerIsAttacking
+        && _playerIsIntersecting
+        && !_context.IsHurt)
+      .WithAction(() =>
+      {
+        _context.IsHit = true;
+        _context.IsHurt = true;
+        _context.Health = -1;
+
+        return PipelineStepResult.Continue;
+      })
+      .Build();
   }
 }
