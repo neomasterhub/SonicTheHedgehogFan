888596d8625b7a674beed02c228a7ab778becd98
Neomaster X <neomasterx@outlook.com>
2026-06-23 04:34:58 +0300

    #599 IBlock.SpeedX, SpeedY getters

--- Assets/Scripts/Objects/Block/IBlock.cs
+++ Assets/Scripts/Objects/Block/IBlock.cs
@@ -3,6 +3,8 @@ public interface IBlock
   bool IsHit { get; }
   bool IsHurt { get; }
   int Health { get; }
+  float SpeedX { get; }
+  float SpeedY { get; }
   float LeftPushSpeed { get; }
   float RightPushSpeed { get; }
   WallDetectionResult? LeftWall { get; }
--- Assets/Scripts/Objects/Block/IBlockContext.cs
+++ Assets/Scripts/Objects/Block/IBlockContext.cs
@@ -1,8 +1,6 @@
 public interface IBlockContext : IBlock
 {
   float Speed { get; set; }
-  float SpeedX { get; set; }
-  float SpeedY { get; set; }
   IBlockPlayer Player { get; }
   GroundDetectionResult? Ground { get; set; }
 }
