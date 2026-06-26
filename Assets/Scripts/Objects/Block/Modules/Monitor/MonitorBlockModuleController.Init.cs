1f4852cee1ebe7a15f4910dfd28d9effdadc011d
Neomaster X <neomasterx@outlook.com>
2026-06-26 07:14:32 +0300

    #594 Monitor script: init components

--- Assets/Scripts/Objects/Block/Modules/Monitor/MonitorBlockModuleController.Init.cs
+++ Assets/Scripts/Objects/Block/Modules/Monitor/MonitorBlockModuleController.Init.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using UnityEngine;
+
 /// <summary>
 /// Init.
 /// </summary>
@@ -6,5 +9,28 @@ public partial class MonitorBlockModuleController
   public override void Initialize(BlockControllerBase context)
   {
     base.Initialize(context);
+
+    InitializeComponents();
+  }
+
+  private void InitializeComponents()
+  {
+    _animator = GetComponent<Animator>();
+
+    _positionModule = GetComponent<NormalPositionBlockModuleController>();
+    _sensorModule = GetComponent<ABSensorBlockModuleController>();
+    _speedModule = GetComponent<GroundSpeedBlockModuleController>();
+
+    _otherModules = GetComponents<BlockModuleControllerBase>()
+      .Where(m => m.enabled
+        && m != this
+        && m != _positionModule
+        && m != _sensorModule
+        && m != _speedModule)
+      .ToArray();
+
+    var screen = transform.Find("Screen");
+    _screenObj = screen.gameObject;
+    _screenAnimator = screen.GetComponent<Animator>();
   }
 }
--- Assets/Scripts/Objects/Block/Modules/Monitor/MonitorBlockModuleController.cs
+++ Assets/Scripts/Objects/Block/Modules/Monitor/MonitorBlockModuleController.cs
@@ -1,7 +1,22 @@
+using UnityEngine;
+
 /// <summary>
 /// Data.
 /// </summary>
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(ABSensorBlockModuleController))]
+[RequireComponent(typeof(GroundSpeedBlockModuleController))]
+[RequireComponent(typeof(NormalPositionBlockModuleController))]
 public partial class MonitorBlockModuleController
   : BlockModuleControllerBase
 {
+  private Animator _animator;
+
+  private GameObject _screenObj;
+  private Animator _screenAnimator;
+
+  private ABSensorBlockModuleController _sensorModule;
+  private GroundSpeedBlockModuleController _speedModule;
+  private NormalPositionBlockModuleController _positionModule;
+  private BlockModuleControllerBase[] _otherModules;
 }
