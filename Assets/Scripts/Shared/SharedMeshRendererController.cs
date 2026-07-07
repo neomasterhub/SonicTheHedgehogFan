a15d6b99cf2b1ea6a997c3be71df92323013296b
Neomaster X <neomasterx@outlook.com>
2026-07-07 18:12:13 +0300

    #624 SharedMeshRendererController: draw polygon

--- Assets/Scripts/Shared/SharedMeshRendererController.cs
+++ Assets/Scripts/Shared/SharedMeshRendererController.cs
@@ -98,4 +98,29 @@ public class SharedMeshRendererController : MonoBehaviour
     _colors.Clear();
     _triangles.Clear();
   }
+
+  public void DrawPolygon(Vector3[] points, Color color, float alpha = 1)
+  {
+    if (points == null || points.Length < 3)
+    {
+      return;
+    }
+
+    color.a = alpha;
+
+    var vi = _vertices.Count;
+
+    for (var i = 0; i < points.Length; i++)
+    {
+      _vertices.Add(points[i]);
+      _colors.Add(color);
+    }
+
+    for (var i = 1; i < points.Length - 1; i++)
+    {
+      _triangles.Add(vi);
+      _triangles.Add(vi + i);
+      _triangles.Add(vi + i + 1);
+    }
+  }
 }
