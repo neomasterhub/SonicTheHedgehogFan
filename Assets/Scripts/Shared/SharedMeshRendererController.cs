180d894567d32477145247266eba60223b7ea68e
Neomaster X <neomasterx@outlook.com>
2026-07-07 17:39:14 +0300

    #624 SharedMeshRendererController: material, mesh

--- Assets/Scripts/Shared/SharedMeshRendererController.cs
+++ Assets/Scripts/Shared/SharedMeshRendererController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class SharedMeshRendererController : MonoBehaviour
 {
+  private Material _material;
+  private Mesh _mesh;
+
   public static SharedMeshRendererController Instance { get; private set; }
 
   private void Awake()
@@ -13,6 +16,8 @@ public class SharedMeshRendererController : MonoBehaviour
     }
 
     Instance = this;
+
+    InitializeComponents();
   }
 
   private void OnDestroy()
@@ -20,6 +25,38 @@ public class SharedMeshRendererController : MonoBehaviour
     if (Instance == this)
     {
       Instance = null;
+    }
+
+    DestroyComponents();
+  }
+
+  private void InitializeComponents()
+  {
+    _material = new(Shader.Find("Sprites/Default"))
+    {
+      hideFlags = HideFlags.HideAndDontSave,
+    };
+
+    _mesh = new()
+    {
+      hideFlags = HideFlags.HideAndDontSave,
+    };
+
+    _mesh.MarkDynamic();
+  }
+
+  private void DestroyComponents()
+  {
+    if (_material != null)
+    {
+      Destroy(_material);
+      _material = null;
+    }
+
+    if (_mesh != null)
+    {
+      Destroy(_mesh);
+      _mesh = null;
     }
   }
 }
