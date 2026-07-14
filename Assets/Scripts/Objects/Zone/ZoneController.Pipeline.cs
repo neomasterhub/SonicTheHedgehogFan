c2abb29f333814e255890bb6e407a0f14bb11d7b
Neomaster X <neomasterx@outlook.com>
2026-07-14 15:12:14 +0300

    #632 fix: ZoneController: flag applying

--- Assets/Scripts/Objects/Zone/ZoneController.Pipeline.cs
+++ Assets/Scripts/Objects/Zone/ZoneController.Pipeline.cs
@@ -5,7 +5,7 @@ public partial class ZoneController
 {
   private void LateUpdate()
   {
-    _zoneObject.IntersectingZones.Set(
+    _zoneObject.IntersectingZones = _zoneObject.IntersectingZones.Set(
       _zoneType,
       _zoneCollider.bounds.Intersects(_zoneObjectCollider.bounds));
   }
