1ff83acc22e68f4890f6bbddae31ed0e6aa788fb
Neomaster X <neomasterx@outlook.com>
2026-08-07 11:35:55 +0300

    #657 Spin smoke: UpdateVisibility()

--- Assets/Scripts/Objects/Smoke/SpinSmokeController.Pipeline.cs
+++ Assets/Scripts/Objects/Smoke/SpinSmokeController.Pipeline.cs
@@ -1,6 +1,39 @@
+using UnityEngine;
+
 /// <summary>
 /// Pipeline.
 /// </summary>
 public partial class SpinSmokeController
 {
+  private void FixedUpdate()
+  {
+    UpdateVisibility();
+  }
+
+  private void UpdateVisibility()
+  {
+    var psm = _particleSystem.main;
+    var color = psm.startColor.color;
+
+    if (_source == null)
+    {
+      if (color.a == 0)
+      {
+        return;
+      }
+
+      color.a = Mathf.Max(0, color.a - _alphaStep);
+      psm.startColor = new ParticleSystem.MinMaxGradient(color);
+    }
+    else
+    {
+      if (color.a == _alphaMax)
+      {
+        return;
+      }
+
+      color.a = Mathf.Min(_alphaMax, color.a + _alphaStep);
+      psm.startColor = new ParticleSystem.MinMaxGradient(color);
+    }
+  }
 }
