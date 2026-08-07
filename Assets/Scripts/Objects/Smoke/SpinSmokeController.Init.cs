bab881e59c17e0045ba8d9091eca08eb5383cf01
Neomaster X <neomasterx@outlook.com>
2026-08-07 11:06:06 +0300

    #657 Spin smoke: ctor

--- Assets/Scripts/Objects/Smoke/SpinSmokeController.Init.cs
+++ Assets/Scripts/Objects/Smoke/SpinSmokeController.Init.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using static SharedConsts;
+using static SpinSmokeConsts;
 
 /// <summary>
 /// Init.
 /// </summary>
 public partial class SpinSmokeController
 {
+  public SpinSmokeController()
+  {
+    _alphaMax = AlphaMax;
+    _alphaStep = AlphaStep;
+  }
+
   private void Awake()
   {
     _particleSystem = GetComponent<ParticleSystem>();
